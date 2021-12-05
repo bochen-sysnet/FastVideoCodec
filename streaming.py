d31dc445a020b045093026a303d92accee62d1fc
Bo Chen <bochen1993cs@gmail.com>
2021-12-05 10:07:34 -0600

    simple test

--- streaming.py
+++ streaming.py
@@ -104,38 +104,6 @@ class VideoDataset(Dataset):
             # When everything done, release the video capture object
             cap.release()
         #print("[log] Total frames: ", self._total_frames)
-        
-class FrameDataset(Dataset):
-    def __init__(self, root_dir, frame_size=None):
-        self._dataset_dir = os.path.join(root_dir,'vimeo_septuplet','sequences')
-        self._train_list_dir = os.path.join(root_dir,'vimeo_septuplet','sep_trainlist.txt')
-        self._frame_size = frame_size
-        self._total_frames = 0 # Storing file names in object
-        self.get_septuplet_names()
-        
-    def get_septuplet_names(self):
-        print("[log] Looking for septuplets in", self._dataset_dir) 
-        self.__septuplet_names = []
-        with open(self._train_list_dir,'r') as f:
-            for line in f:
-                line = line.strip()
-                self.__septuplet_names += [self._dataset_dir + '/' + line]
-        print("[log] Number of septuplets found {}".format(len(self.__septuplet_names)))
-                
-    def __len__(self):
-        return len(self.__septuplet_names)*7
-        
-    def reset(self):
-        return
-        
-    def __getitem__(self, idx):
-        frame_idx,tuplet_idx = idx%7+1,idx//7
-        base_dir = self.__septuplet_names[tuplet_idx]
-        img_dir = base_dir+'/'+f'im{frame_idx}.png'
-        img = Image.open(img_dir).convert('RGB')
-        if self._frame_size is not None:
-            img = img.resize(self._frame_size) 
-        return img,frame_idx==7
 
 class AverageMeter(object):
     """Computes and stores the average and current value"""
@@ -224,93 +192,8 @@ else:
     print("Cannot load model codec", CODEC_NAME)
     exit(1)
 print("===================================================================")
-        
-def train(epoch, model, train_dataset, optimizer, test_dataset, best_codec_score):
-    aux_loss_module = AverageMeter()
-    img_loss_module = AverageMeter()
-    be_loss_module = AverageMeter()
-    psnr_module = AverageMeter()
-    msssim_module = AverageMeter()
-    all_loss_module = AverageMeter()
-    scaler = torch.cuda.amp.GradScaler(enabled=True)
-    batch_size = 7
-    ds_size = len(train_dataset)
     
-    model.train()
-    update_training(model,epoch)
-    
-    train_iter = tqdm(range(ds_size))
-    data = []
-    batch_idx = 0
-    for data_idx,_ in enumerate(train_iter):
-        # ----------TRAIN-----------
-        frame,eof = train_dataset[data_idx]
-        data.append(transforms.ToTensor()(frame))
-        if len(data) < batch_size and not eof:
-            continue
-        data = torch.stack(data, dim=0).cuda()
-        l = data.size(0)-1
-        
-        # run model
-        _,img_loss_list,bpp_est_list,aux_loss_list,psnr_list,msssim_list,_ = parallel_compression(model,data,True)
-        
-        # aggregate loss
-        be_loss = torch.stack(bpp_est_list,dim=0).mean(dim=0)
-        aux_loss = torch.stack(aux_loss_list,dim=0).mean(dim=0)
-        img_loss = torch.stack(img_loss_list,dim=0).mean(dim=0)
-        psnr = torch.stack(psnr_list,dim=0).mean(dim=0)
-        msssim = torch.stack(msssim_list,dim=0).mean(dim=0)
-        loss = model.loss(img_loss,be_loss,aux_loss)
-        
-        # record loss
-        aux_loss_module.update(aux_loss.cpu().data.item(), l)
-        img_loss_module.update(img_loss.cpu().data.item(), l)
-        be_loss_module.update(be_loss.cpu().data.item(), l)
-        psnr_module.update(psnr.cpu().data.item(),l)
-        msssim_module.update(msssim.cpu().data.item(), l)
-        all_loss_module.update(loss.cpu().data.item(), l)
-        
-        # backward
-        scaler.scale(loss).backward()
-        # update model after compress each video
-        if eof:
-            scaler.step(optimizer)
-            scaler.update()
-            optimizer.zero_grad()
-            
-        # show result
-        train_iter.set_description(
-            f"{data_idx:6}. "
-            f"IL: {img_loss_module.val:.2f} ({img_loss_module.avg:.2f}). "
-            f"BE: {be_loss_module.val:.2f} ({be_loss_module.avg:.2f}). "
-            f"AX: {aux_loss_module.val:.2f} ({aux_loss_module.avg:.2f}). "
-            f"AL: {all_loss_module.val:.2f} ({all_loss_module.avg:.2f}). "
-            f"P: {psnr_module.val:.2f} ({psnr_module.avg:.2f}). "
-            f"M: {msssim_module.val:.4f} ({msssim_module.avg:.4f}). ")
-
-        # clear result every 1000 batches
-        if batch_idx % 10000 == 0 and batch_idx>0: # From time to time, reset averagemeters to see improvements
-            loss_module.reset_meters()
-            img_loss_module.reset()
-            aux_loss_module.reset()
-            be_loss_module.reset()
-            all_loss_module.reset()
-            psnr_module.reset()
-            msssim_module.reset()   
-
-        
-        # ---------EVALUATE---------
-        if batch_idx % 10000 == 0 and batch_idx > 0:
-            best_codec_score = test(model, test_dataset, best_codec_score)
-        # --------------------------
-            
-        # clear input
-        data = []
-        batch_idx += 1
-        
-    return test(model, test_dataset, best_codec_score)
-    
-def test(model, test_dataset, best_codec_score=None):
+def streaming(model, test_dataset):
     aux_loss_module = AverageMeter()
     img_loss_module = AverageMeter()
     ba_loss_module = AverageMeter()
@@ -379,18 +262,6 @@ def test(model, test_dataset, best_codec_score=None):
         data = []
         
     test_dataset.reset()
-    
-    # evaluate
-    score = [ba_loss_module.avg,psnr_module.avg,msssim_module.avg]
-    is_best = isinstance(best_codec_score,list) and (score[0] <= best_codec_score[0]) and (score[1] >= best_codec_score[1])
-    if is_best:
-        print("New best score is achieved: ", score, ". Previous score was: ", best_codec_score)
-        best_codec_score = score
-    state = {'epoch': epoch, 'state_dict': model.state_dict(), 'score': score}
-    save_checkpoint(state, is_best, BACKUP_DIR, CODEC_NAME)
-    print('Weights are saved to backup directory: %s' % (BACKUP_DIR))
-    
-    return best_codec_score
         
 def test_x26x(test_dataset,name='x264'):
     print('Benchmarking:',name)
@@ -432,29 +303,14 @@ def test_x26x(test_dataset,name='x264'):
             data = []
             
         test_dataset.reset()
-                        
-def adjust_learning_rate(optimizer, epoch):
-    """Sets the learning rate to the initial LR decayed by 10 every 30 epochs"""
-    LEARNING_RATE = 1e-4
-    LR_DECAY_RATE = 0.1
-    STEPS = []
-    lr_new = LEARNING_RATE * (LR_DECAY_RATE ** (sum(epoch >= np.array(STEPS))))
-    for param_group in optimizer.param_groups:
-        param_group['lr'] = lr_new
-    return lr_new
-    
+        
 ####### Load dataset
-train_dataset = FrameDataset('../dataset/vimeo')
 test_dataset = VideoDataset('../dataset/UVG', frame_size=(256,256))
 
 # optionaly try x264,x265
 #test_x26x(test_dataset,'x264')
 #test_x26x(test_dataset,'x265')
 
-for epoch in range(BEGIN_EPOCH, END_EPOCH + 1):
-    # Adjust learning rate
-    r = adjust_learning_rate(optimizer, epoch)
-    
-    # Train and test model
-    print('training at epoch %d, r=%.2f' % (epoch,r))
-    best_codec_score = train(epoch, model, train_dataset, optimizer, test_dataset, best_codec_score)
+# Train and test model
+print('training at epoch %d, r=%.2f' % (epoch,r))
+streaming(model, test_dataset)
