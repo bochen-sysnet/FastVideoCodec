cc413d8ca2145a69c4bc02563f2ca56cb6ee0a93
Bo Chen <bochen1993cs@gmail.com>
2021-12-07 12:12:00 -0600

    Update streaming.py

--- streaming.py
+++ streaming.py
@@ -144,9 +144,17 @@ def test_x26x(test_dataset, name='x264'):
         Q = 27#15,19,23,27
         GOP = 13
         output_filename = 'tmp/videostreams/output.mp4'
-        cmd = f'/usr/bin/ffmpeg -y -s {width}x{height} -pixel_format bgr24 -f rawvideo -r {fps} -i pipe: -vcodec libx264 -pix_fmt yuv420p '+\
-                f'-preset veryfast -tune zerolatency -crf {Q} -g {GOP} -bf 2 -b_strategy 0 -sc_threshold 0 -loglevel debug '+\
-                f'-rtsp_transport tcp -f rtsp rtsp://127.0.0.1:8555/live'
+        if name == 'x265':
+            cmd = f'/usr/bin/ffmpeg -y -s {width}x{height} -pixel_format bgr24 -f rawvideo -r {fps} -i pipe: -vcodec libx265 -pix_fmt yuv420p '+\
+                    f'-preset veryfast -tune zerolatency -x265-params "crf={Q}:keyint={GOP}:verbose=1" '+\
+                    f'-rtsp_transport tcp -f rtsp rtsp://127.0.0.1:8555/live'
+        elif name == 'x264':
+            cmd = f'/usr/bin/ffmpeg -y -s {width}x{height} -pixel_format bgr24 -f rawvideo -r {fps} -i pipe: -vcodec libx264 -pix_fmt yuv420p '+\
+                    f'-preset veryfast -tune zerolatency -crf {Q} -g {GOP} -bf 2 -b_strategy 0 -sc_threshold 0 -loglevel debug '+\
+                    f'-rtsp_transport tcp -f rtsp rtsp://127.0.0.1:8555/live'
+        else:
+            print('Codec not supported')
+            exit(1)
         process = sp.Popen(shlex.split(cmd), stdin=sp.PIPE, stdout=sp.DEVNULL, stderr=sp.STDOUT)
         for idx,img in enumerate(raw_clip):
             img = np.array(img)
@@ -169,6 +177,9 @@ def test_x26x(test_dataset, name='x264'):
             
         # Open sub-process that gets in_stream as input and uses stdout as an output PIPE.
         p1 = sp.Popen(command, stdout=sp.PIPE)
+        
+        # Beginning time of streaming
+        t_0 = time.perf_counter()
         
         # Start a thread that streams data
         threading.Thread(target=stream_data, args=(data,)).start() 
@@ -197,6 +208,9 @@ def test_x26x(test_dataset, name='x264'):
             msssim_list += [MSSSIM(raw, com)]
             print(i,psnr_list[-1])
             i += 1
+        total_time = time.perf_counter() - t_0
+        fps = i/total_time
+        print(total_time,fps)
         return psnr_list,msssim_list
             
     from collections import deque
