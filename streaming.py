f022d5c24ffa95246e98fa8faad05ce4166f8df3
Bo Chen <bochen1993cs@gmail.com>
2021-12-07 12:31:48 -0600

    Update streaming.py

--- streaming.py
+++ streaming.py
@@ -159,6 +159,7 @@ def test_x26x(test_dataset, name='x264'):
         for idx,img in enumerate(raw_clip):
             img = np.array(img)
             process.stdin.write(img.tobytes())
+            time.sleep(1/30.)
         # Close and flush stdin
         process.stdin.close()
         # Wait for sub-process to finish
