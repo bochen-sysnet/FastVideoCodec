6811641121908a0d37dcc867fb18c4a33cbb87ef
Bo Chen <bochen1993cs@gmail.com>
2021-12-09 12:06:29 -0600

    Update main.py

--- main.py
+++ main.py
@@ -357,7 +357,7 @@ best_codec_score = [1,0,0]
 if CODEC_NAME in ['x265', 'x264', 'RAW']:
     # nothing to load
     print("No need to load for ", CODEC_NAME)
-elif CODEC_NAME in ['SCVC']:
+elif CODEC_NAME in ['SCVC','SPVC']:
     # load what exists
     print("Load whatever exists for",CODEC_NAME)
     pretrained_model_path = "backup/RLVC/RLVC-2P_best.pth"
