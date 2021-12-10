74da79c94627bd2b86ca461959042cfbc6cf19cf
Bo Chen <bochen1993cs@gmail.com>
2021-12-10 16:11:37 -0600

    Update main.py

--- main.py
+++ main.py
@@ -26,10 +26,11 @@ from dataset import VideoDataset, FrameDataset
 
 # OPTION
 BACKUP_DIR = 'backup'
-CODEC_NAME = 'SPVC'
+CODEC_NAME = 'DVC'
 loss_type = 'P'
 compression_level = 2 # 0,1,2,3
-RESUME_CODEC_PATH = f'backup/{CODEC_NAME}/{CODEC_NAME}-{compression_level}{loss_type}_ckpt.pth'
+RESUME_CODEC_PATH = 'backup/SPVC/SPVC-2P_ckpt.pth'
+#RESUME_CODEC_PATH = f'backup/{CODEC_NAME}/{CODEC_NAME}-{compression_level}{loss_type}_ckpt.pth'
 #RESUME_CODEC_PATH = '../YOWO/backup/ucf24/yowo_ucf24_16f_SPVC_ckpt.pth'
 LEARNING_RATE = 0.0001
 WEIGHT_DECAY = 5e-4
@@ -75,7 +76,6 @@ def train(epoch, model, train_dataset, optimizer, best_codec_score, test_dataset
     
     train_iter = tqdm(train_loader)
     for batch_idx,data in enumerate(train_iter):
-        if batch_idx<=45000 and epoch==1:continue
         data = data[0].cuda()
         l = data.size(0)-1
         
