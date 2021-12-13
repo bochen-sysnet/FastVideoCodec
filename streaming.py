ef1bd913a20aae53978fc5df861cec1cc65c012a
Bo Chen <bochen1993cs@gmail.com>
2021-12-12 18:06:46 -0600

    add argparse

--- streaming.py
+++ streaming.py
@@ -295,11 +295,11 @@ def dynamic_simulation_x26x(test_dataset, name='x264'):
         if name == 'x265':
             cmd = f'/usr/bin/ffmpeg -y -s {width}x{height} -pixel_format bgr24 -f rawvideo -r {fps} -i pipe: -vcodec libx265 -pix_fmt yuv420p '+\
                     f'-preset veryfast -tune zerolatency -x265-params "crf={Q}:keyint={GOP}:verbose=1" '+\
-                    f'-rtsp_transport tcp -f rtsp rtsp://127.0.0.1:8555/live'
+                    f'-rtsp_transport tcp -f rtsp rtsp://127.0.0.1:8888/live'
         elif name == 'x264':
             cmd = f'/usr/bin/ffmpeg -y -s {width}x{height} -pixel_format bgr24 -f rawvideo -r {fps} -i pipe: -vcodec libx264 -pix_fmt yuv420p '+\
                     f'-preset veryfast -tune zerolatency -crf {Q} -g {GOP} -bf 2 -b_strategy 0 -sc_threshold 0 -loglevel debug '+\
-                    f'-rtsp_transport tcp -f rtsp rtsp://127.0.0.1:8555/live'
+                    f'-rtsp_transport tcp -f rtsp rtsp://127.0.0.1:8888/live'
         else:
             print('Codec not supported')
             exit(1)
@@ -325,7 +325,7 @@ def dynamic_simulation_x26x(test_dataset, name='x264'):
         # ssh -R 8555:localhost:8555 uiuc@192.168.251.195
         command = ['/usr/bin/ffmpeg',
             '-rtsp_flags', 'listen',
-            '-i', 'rtsp://127.0.0.1:8555/live?tcp?',
+            '-i', 'rtsp://127.0.0.1:8888/live?tcp?',
             '-f', 'image2pipe',    # Use image2pipe demuxer
             '-pix_fmt', 'bgr24',   # Set BGR pixel format
             '-vcodec', 'rawvideo', # Get rawvideo output format.
@@ -816,3 +816,11 @@ test_dataset = VideoDataset('UVG', frame_size=(256,256))
 #streaming_SPVC('SPVC-stream', test_dataset)
 #streaming_RLVC_DVC('DVC', test_dataset)
 streaming_RLVC_DVC('RLVC', test_dataset)
+
+if __name__ == '__main__':
+    import argparse
+
+    parser = argparse.ArgumentParser(description='Parameters of simulations.')
+    parser.add_argument('--role', type=str, default='Server', help='Server or Client')
+    args = parser.parse_args()
+    print(args.role)
