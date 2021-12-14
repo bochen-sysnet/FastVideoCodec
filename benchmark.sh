ad4bcbfe2b5e1903b62ac98352d3667458b672b9
Bo Chen <bochen1993cs@gmail.com>
2021-12-13 20:05:39 -0600

    d

--- benchmark.sh
+++ benchmark.sh
@@ -5,6 +5,6 @@
   
 #python3 streaming.py --task x265 --server_port 8184 --probe_port 8181
 
-python3 streaming.py --task RLVC --server_port 8186
+python3 streaming.py --task RLVC --server_port 8088
   
-python3 streaming.py --task DVC --server_port 8186
+python3 streaming.py --task DVC --server_port 8088
