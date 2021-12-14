7246c69e82d0fd51084a3a11c5bf59033d9a5b4c
Bo Chen <bochen1993cs@gmail.com>
2021-12-13 19:22:16 -0600

    Update benchmark.sh

--- benchmark.sh
+++ benchmark.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 # X264
-python3 streaming.py --task x264 --server_port 8184 --probe_port 8181
+#python3 streaming.py --task x264 --server_port 8184 --probe_port 8181
   
-python3 streaming.py --task x265 --server_port 8184 --probe_port 8181
+#python3 streaming.py --task x265 --server_port 8184 --probe_port 8181
 
 python3 streaming.py --task RLVC --server_port 8184 --probe_port 8181
   
