e2ab9bb91fede291d9b01dc557658d6167574dca
Bo Chen <bochen1993cs@gmail.com>
2021-12-17 17:01:31 -0600

    minor

--- benchmark.sh
+++ benchmark.sh
@@ -1,8 +1,22 @@
 #!/bin/bash
 
-echo "No loss"
-# python eval.py --task x264 --role client &
-# python eval.py --task x264 --role server
+# echo "Local encoder test"
+
+# python eval.py --task x264
+
+# python eval.py --task x265
+
+# python eval.py --task DVC --encoder_test
+
+# python eval.py --task RLVC --encoder_test
+
+# python eval.py --task SPVC --encoder_test
+
+##############################################
+
+echo "No loss remote test on 130.126.136.154"
+ssh monet@130.126.136.154 "/home/monet/anaconda3/bin/activate yolov5;cd research/FastVideoCodec;python eval.py --task x264 --role client" &
+python eval.py --task x264 --role server
   
 # python eval.py --task x265 --role client &
 # python eval.py --task x265 --role server
@@ -13,12 +27,14 @@ echo "No loss"
 # python eval.py --task DVC --role client &
 # python eval.py --task DVC --role server
 
-python eval.py --task SPVC --role client &
-python eval.py --task SPVC --role server
+# python eval.py --task SPVC --role client &
+# python eval.py --task SPVC --role server
 
 echo "Loss: 10%"
-# 10% packet loss
-sudo tc qdisc add dev lo root netem loss 10%
+# sudo tc qdisc add dev lo root netem loss 10%
 
-# remove loss
-sudo tc qdisc del dev lo root
+# sudo tc qdisc del dev lo root
+
+##############################################
+
+echo "No loss remote test on 10.251.114.121"
