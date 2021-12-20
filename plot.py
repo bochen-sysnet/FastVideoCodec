90b5350f8214c507dfafd2c6fd6ed4176a3bf913
Bo Chen <bochen1993cs@gmail.com>
2021-12-19 22:02:36 -0600

    plot

--- plot.py
+++ plot.py
@@ -38,7 +38,8 @@ bpps = [[0.14,0.21,0.33,0.5],
 		[0.16,0.25,0.39,0.59],
 		[0.12,0.187],
 		[0.138,0.154],
-		[0.123,0.181,0.284,0.3925]]
+		# [0.123,0.181,0.284,0.3925]
+		]
 PSNRs = [[28.37,29.96,31.31,32.38],
 		[29.56,30.90,31.96,32.82],
 		[26.19,30.82],
@@ -57,7 +58,7 @@ com_t = [[0.031189141000140808, 0.061611389999825406, 0.08575277299974005, 0.112
 [0.03548506700008147, 0.07703001000004406, 0.11634391000029609, 0.1602356679998138, 0.20212238199974308, 0.24543897899980038, 0.28723739499992007, 0.33600276399988616, 0.3826641949999612, 0.42505351599993446, 0.4697764459995142, 0.5151504109999223, 0.5607268250005291, 0.6026666810003007],
 [0.04606491999993523, 0.06779569900027127, 0.09685762100025386, 0.12219671000002563, 0.14586037300023236, 0.1700412850000248, 0.21632630700014488, 0.23113141200019527, 0.2638085839998894, 0.2760139719998733, 0.3001248149998901, 0.30320839800015165, 0.32136949599998843, 0.3438082170000598]
 ]
-image_nums = [range(1,15) for _ in range(3)]
+image_nums = [range(1,15) for _ in range(2)]
 line_plot(image_nums,com_t,ml_labels,
 		'/home/bo/Dropbox/Research/SIGCOMM22/images/scalability.eps',
 		'Number of images','Time (s)',xticks=[0,5,10,15])
@@ -73,7 +74,7 @@ def bar_plot(avg,std,path,color,ylabel,yticks=None):
 		yerr=std, error_kw=dict(lw=1, capsize=1, capthick=1))
 	ax.set_ylabel(ylabel, fontsize = labelsize)
 	ax.set_xticks(ind)
-	ax.set_xticklabels(labels)
+	ax.set_xticklabels(labels[:N])
 	if yticks is not None:
 		plt.yticks( yticks )
 	xleft, xright = ax.get_xlim()
@@ -83,8 +84,8 @@ def bar_plot(avg,std,path,color,ylabel,yticks=None):
 	plt.tight_layout()
 	fig.savefig(path,bbox_inches='tight')
 
-com_speeds_avg = [56.96,57.35,19.31,27.90,32.89] # 27.07,32.89,36.83
-com_speeds_std = [1.96,1.35,1.31,1.90,1.84]
+com_speeds_avg = [56.96,57.35,19.31,27.90]#,32.89] # 27.07,32.89,36.83
+com_speeds_std = [1.96,1.35,1.31,1.90]#,1.84]
 bar_plot(com_speeds_avg,com_speeds_std,
 		'/home/bo/Dropbox/Research/SIGCOMM22/images/speed.eps',
 		colors[1],'Speed (fps)',yticks=np.arange(0,70,15))
