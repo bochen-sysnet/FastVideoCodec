7092f63b4837624d1fabae62548296b26f3d8bb1
Bo Chen <bochen1993cs@gmail.com>
2021-12-19 21:49:57 -0600

    train dvc 3

--- main.py
+++ main.py
@@ -25,7 +25,7 @@ from models import load_state_dict_whatever, load_state_dict_all, load_state_dic
 from dataset import VideoDataset, FrameDataset
 
 # OPTION
-CODEC_NAME = 'RLVC'
+CODEC_NAME = 'DVC'
 SAVE_DIR = f'backup/{CODEC_NAME}'
 loss_type = 'P'
 compression_level = 3 # 0,1,2,3
@@ -71,7 +71,7 @@ if CODEC_NAME in ['x265', 'x264', 'RAW']:
     print("No need to load for ", CODEC_NAME)
 elif CODEC_NAME in ['DVC','RLVC']:
     # load what exists
-    pretrained_model_path = "backup/RLVC/RLVC-3P_ckpt.pth"
+    pretrained_model_path = "backup/DVC/DVC-3P_ckpt.pth"
     checkpoint = torch.load(pretrained_model_path)
     best_codec_score = checkpoint['score']
     load_state_dict_whatever(model, checkpoint['state_dict'])
