87f02a4cb75e45b35a9c76c128882a037d66791a
Bo Chen <bochen1993cs@gmail.com>
2021-12-29 23:35:25 -0600

    d

--- main.py
+++ main.py
@@ -73,7 +73,7 @@ best_codec_score = [1,0,0]
 if CODEC_NAME in ['x265', 'x264', 'RAW']:
     # nothing to load
     print("No need to load for ", CODEC_NAME)
-elif CODEC_NAME in ['DVC']:
+elif CODEC_NAME in []:
     # load what exists
     pretrained_model_path = "backup/DVC/DVC-3P_ckpt.pth"
     checkpoint = torch.load(pretrained_model_path)
