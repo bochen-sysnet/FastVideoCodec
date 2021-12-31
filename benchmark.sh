1acc818135074b8a01564a4a3ea3e5b64c40b5fd
Bo Chen <bochen1993cs@gmail.com>
2021-12-31 17:28:26 -0600

    eval

--- benchmark.sh
+++ benchmark.sh
@@ -7,15 +7,15 @@ do
 	    echo ">>>>>>>>>>>Enable: $lr loss<<<<<<<<<<<<"
 	    sudo tc qdisc add dev wlp68s0 root netem loss $lr
 	fi
-	# for test_num in 1 2 3 #4 5 
-	# do
-	# 	echo "Role:$1. IP:$2. Test: $test_num. Loss: $lr"	
-	# 	for task in x264 x265 RLVC DVC SPVC96
-	# 	do
-	# 		python3 eval.py --task $task --role $1 --server_ip $2 
-	# 		# sudo kill -9 `sudo lsof -t -i:8846`
-	# 	done
-	# done
+	for test_num in 1 2 3 #4 5 
+	do
+		echo "Role:$1. IP:$2. Test: $test_num. Loss: $lr"	
+		for task in x264 x265 RLVC DVC SPVC96
+		do
+			python3 eval.py --task $task --role $1 --server_ip $2 
+			# sudo kill -9 `sudo lsof -t -i:8846`
+		done
+	done
 	if [ $1 == 'client' ] | [ $1 == 'standalone' ]
 	then
 	    echo ">>>>>>>>>>>Disable: $lr loss<<<<<<<<<<<<"
