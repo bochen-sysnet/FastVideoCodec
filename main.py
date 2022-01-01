6a61bf3b9c2dfa9e9b31681ebb5e2b69ae756508
Bo Chen <bochen1993cs@gmail.com>
2022-01-01 17:54:27 -0600

    Update main.py

--- main.py
+++ main.py
@@ -180,19 +180,19 @@ def train(epoch, model, train_dataset, optimizer, best_codec_score, test_dataset
             msssim_module.reset()   
             
         if batch_idx % 1000 == 0 and batch_idx>0:
-            print('testing at batch_idx %d' % (batch_idx))
-            score = test(epoch, model, test_dataset)
+            #print('testing at batch_idx %d' % (batch_idx))
+            #score = test(epoch, model, test_dataset)
             
-            is_best = score[0] <= best_codec_score[0] or score[1] >= best_codec_score[1]
-            if is_best:
-                print("New best score is achieved: ", score, ". Previous score was: ", best_codec_score)
-                best_codec_score = score
-            state = {'epoch': epoch, 'state_dict': model.state_dict(), 'score': score}
-            save_checkpoint(state, is_best, SAVE_DIR, CODEC_NAME, loss_type, compression_level)
-            #state = {'epoch': epoch, 'state_dict': model.state_dict(), 'score': best_codec_score}
-            #save_checkpoint(state, False, SAVE_DIR, CODEC_NAME, loss_type, compression_level)
+            #is_best = score[0] <= best_codec_score[0] or score[1] >= best_codec_score[1]
+            #if is_best:
+            #    print("New best score is achieved: ", score, ". Previous score was: ", best_codec_score)
+            #    best_codec_score = score
+            #state = {'epoch': epoch, 'state_dict': model.state_dict(), 'score': score}
+            #save_checkpoint(state, is_best, SAVE_DIR, CODEC_NAME, loss_type, compression_level)
             #test(epoch, model, test_dataset2)
-            model.train()
+            #model.train()
+            state = {'epoch': epoch, 'state_dict': model.state_dict(), 'score': best_codec_score}
+            save_checkpoint(state, False, SAVE_DIR, CODEC_NAME, loss_type, compression_level)
     return best_codec_score
     
 def test(epoch, model, test_dataset):
