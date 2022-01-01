5e59b4193aa7a74be5617fec854be50f21932b6d
Bo Chen <bochen1993cs@gmail.com>
2021-12-31 22:56:38 -0600

    d

--- main.py
+++ main.py
@@ -73,7 +73,7 @@ best_codec_score = [1,0,0]
 if CODEC_NAME in ['x265', 'x264', 'RAW']:
     # nothing to load
     print("No need to load for ", CODEC_NAME)
-elif CODEC_NAME in ['RLVC']:
+elif CODEC_NAME in []:
     # load what exists
     pretrained_model_path = "backup/RLVC/RLVC-3P_best.pth"
     checkpoint = torch.load(pretrained_model_path)
--- models.py
+++ models.py
@@ -91,7 +91,7 @@ def update_training(model, epoch, batch_idx=None, warmup_epoch=30):
     # setup training weights
     if epoch <= warmup_epoch:
         model.r_img, model.r_bpp, model.r_aux = 1,1,1
-        model.r_rec, model.r_flow, model.r_warp, model.r_mc = 0,0,1,0
+        model.r_rec, model.r_flow, model.r_warp, model.r_mc = 0,1,1,0
     else:
         model.r_img, model.r_bpp, model.r_aux = 1,1,1
         model.r_rec, model.r_flow, model.r_warp, model.r_mc = 1,0,0,0
