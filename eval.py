d0a87f6e98c711977227273e2bc43dd8af494c0d
Bo Chen <bochen1993cs@gmail.com>
2022-01-12 21:18:48 -0600

    show mc wp

--- eval.py
+++ eval.py
@@ -228,6 +228,8 @@ def static_simulation_model(args, test_dataset):
         GoP = args.fP + args.bP +1
         GoP_meters = [AverageMeter() for _ in range(GoP)]
         GoP_meters2 = [AverageMeter() for _ in range(GoP)]
+        WP_meters = [AverageMeter() for _ in range(GoP)]
+        MC_meters = [AverageMeter() for _ in range(GoP)]
         data = []
         test_iter = tqdm(range(ds_size))
         for data_idx,_ in enumerate(test_iter):
@@ -244,7 +246,15 @@ def static_simulation_model(args, test_dataset):
                 if l>args.fP+1:
                     com_imgs,img_loss_list1,bpp_est_list1,aux_loss_list1,psnr_list1,msssim_list1,bpp_act_list1 = parallel_compression(model,torch.flip(data[:args.fP+1],[0]),True)
                     data[args.fP:args.fP+1] = com_imgs[0:1]
+                    for idx,wp in aux_loss_list1:
+                        WP_meters[5-idx].update(wp)
+                    for idx,mc in msssim_list1:
+                        MC_meters[5-idx].update(mc)
                     _,img_loss_list2,bpp_est_list2,aux_loss_list2,psnr_list2,msssim_list2,bpp_act_list2 = parallel_compression(model,data[args.fP:],False)
+                    for idx,wp in aux_loss_list2:
+                        WP_meters[7+idx].update(wp)
+                    for idx,mc in msssim_list2:
+                        MC_meters[7+idx].update(mc)
                     img_loss_list = img_loss_list1[::-1] + img_loss_list2
                     aux_loss_list = aux_loss_list1[::-1] + aux_loss_list2
                     psnr_list = psnr_list1[::-1] + psnr_list2
@@ -292,7 +302,12 @@ def static_simulation_model(args, test_dataset):
         test_dataset.reset()
         psnrs = [float(gm.avg) for gm in GoP_meters]
         bpps = [float(gm.avg) for gm in GoP_meters2]
-        print(lvl,psnrs,bpps)
+        psnrs2 = [float(gm.avg) for gm in MC_meters]
+        psnrs3 = [float(gm.avg) for gm in WP_meters]
+        print(lvl,bpps)
+        print(psnrs)
+        print(psnrs2)
+        print(psnrs3)
     return [ba_loss_module.avg,psnr_module.avg,msssim_module.avg]
 
 def block_until_open(ip_addr,port):
@@ -961,7 +976,7 @@ if __name__ == '__main__':
 
         
     print(args)
-    assert args.dataset in ['UVG','MCL-JCV','Xiph','Xiph2']
+    assert args.dataset in ['UVG','MCL-JCV','Xiph','Xiph2','HEVC']
     test_dataset = VideoDataset('../dataset/'+args.dataset, frame_size=(256,256))
         
     if args.mode == 'dynamic':
