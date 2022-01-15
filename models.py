8964fae40c2360f987e71155c16dbb33efe0e0b5
Bo Chen <bochen1993cs@gmail.com>
2022-01-15 17:16:46 -0600

    Update models.py

--- models.py
+++ models.py
@@ -1276,14 +1276,15 @@ class Warp_net(nn.Module):
         c1_p = self.conv1_p(c1)
         c2 = self.conv2(c1_p)
         c3 = self.conv3(c2)
-        if False:
+        if self.useAttn:
+            c3 = x
             B,C,H,W = x.size()
             frame_pos_emb = self.frame_rot_emb(B,device=x.device)
             image_pos_emb = self.image_rot_emb(H,W,device=x.device)
             x = x.permute(0,2,3,1).reshape(1,-1,C).contiguous()
             x = self.t_attn(x, 'b (f n) d', '(b n) f d', n = H*W, rot_emb = frame_pos_emb) + x
             x = self.s_attn(x, 'b (f n) d', '(b f) n d', f = B, rot_emb = image_pos_emb) + x
-            x = x.view(B,H,W,C).permute(0,3,1,2).contiguous()
+            c3 = x.view(B,H,W,C).permute(0,3,1,2).contiguous()
         c3_u = c1 + bilinearupsacling2(c3)#torch.nn.functional.interpolate(input=c3, scale_factor=2, mode='bilinear', align_corners=True)
         c4 = self.conv4(c3_u)
         c4_u = c0 + bilinearupsacling2(c4)# torch.nn.functional.interpolate(input=c4, scale_factor=2, mode='bilinear', align_corners=True)
