e9f1075623c3f95cfe812395cd9ccb7957bbcb8f
Bo Chen <bochen1993cs@gmail.com>
2022-01-22 08:19:22 -0600

    Update main.py

--- main.py
+++ main.py
@@ -28,7 +28,7 @@ from dataset import VideoDataset, FrameDataset
 CODEC_NAME = 'LSVC-A'
 SAVE_DIR = f'backup/{CODEC_NAME}'
 loss_type = 'P'
-compression_level = 2 # 0,1,2,3
+compression_level = 1 # 0,1,2,3
 RESUME_CODEC_PATH = f'{SAVE_DIR}/{CODEC_NAME}-{compression_level}{loss_type}_ckpt.pth'
 #RESUME_CODEC_PATH = f'{SAVE_DIR}/{CODEC_NAME}-3{loss_type}_ckpt.pth'
 LEARNING_RATE = 0.0001
@@ -36,8 +36,8 @@ WEIGHT_DECAY = 5e-4
 BEGIN_EPOCH = 1
 END_EPOCH = 10
 WARMUP_EPOCH = 5
-device = 1
-STEPS = [0]
+device = 0
+STEPS = [0，0]
 
 if not os.path.exists(SAVE_DIR):
     os.makedirs(SAVE_DIR)
