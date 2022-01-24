72baa13ea1060333a32d450f145e9fa3a1780f22
Bo Chen <bochen1993cs@gmail.com>
2022-01-24 08:25:30 -0600

    Update main.py

--- main.py
+++ main.py
@@ -28,15 +28,15 @@ from dataset import VideoDataset, FrameDataset
 CODEC_NAME = 'SPVC64-N'
 SAVE_DIR = f'backup/{CODEC_NAME}'
 loss_type = 'P'
-compression_level = 3 # 0,1,2,3
-RESUME_CODEC_PATH = f'{SAVE_DIR}/{CODEC_NAME}-{compression_level}{loss_type}_ckpt.pth'
-#RESUME_CODEC_PATH = f'{SAVE_DIR}/{CODEC_NAME}-1{loss_type}_ckpt.pth'
+compression_level = 0 # 0,1,2,3
+#RESUME_CODEC_PATH = f'{SAVE_DIR}/{CODEC_NAME}-{compression_level}{loss_type}_ckpt.pth'
+RESUME_CODEC_PATH = f'{SAVE_DIR}/{CODEC_NAME}-3{loss_type}_ckpt.pth'
 LEARNING_RATE = 0.0001
 WEIGHT_DECAY = 5e-4
 BEGIN_EPOCH = 1
 END_EPOCH = 10
 WARMUP_EPOCH = 5
-device = 0
+device = 1
 STEPS = []
 
 if not os.path.exists(SAVE_DIR):
