635dcc82cf3c62d7f1319243699e4dee0b726d5d
Bo Chen <bochen1993cs@gmail.com>
2022-01-24 20:18:14 -0600

    0

--- benchmark.sh
+++ benchmark.sh
@@ -2,15 +2,15 @@
 
 # impact of network: rebuffer,fps,start-up
 # sudo tc qdisc add dev wlp68s0 root netem loss 20%
-for test_num in 1 2 3 
-do
-	echo "Role:$1. IP:$2. Test: $test_num. Loss: $lr"	
-	for task in SPVC64-N x264 x265 DVC RLVC
-	do
-		python3 eval.py --Q_option Slow --task $task --role $1 --server_ip 130.126.136.154 --client_ip 10.194.246.197
-		# sudo kill -9 `sudo lsof -t -i:8846`
-	done
-done
+# for test_num in 1 2 3 
+# do
+# 	echo "Role:$1. IP:$2. Test: $test_num. Loss: $lr"	
+# 	for task in SPVC64-N x264 x265 DVC RLVC
+# 	do
+# 		python3 eval.py --Q_option Slow --task $task --role $1 --server_ip 130.126.136.154 --client_ip 10.194.246.197
+# 		# sudo kill -9 `sudo lsof -t -i:8846`
+# 	done
+# done
 # sudo tc qdisc del dev wlp68s0 root
 
 # -----------------------------------------------------------
@@ -31,9 +31,12 @@ done
 
 # -----------------------------------------------------------
 # impact of hardware
-# python eval.py --task SPVC64-N,DVC,RLVC --encoder_test --Q_option Slow
-# take the smaller one into account
-# python eval.py --task x265,x264 --fps 1000 --Q_option Slow
+for task in SPVC64-N DVC RLVC
+do
+	python3 eval.py --Q_option Slow --task $task --encoder_test
+done
+python eval.py --task x264 --fps 1000 --Q_option Slow
+python eval.py --task x265 --fps 1000 --Q_option Slow
 
 # -----------------------------------------------------------
 # eval scalability: use different models measure mean,std on UVG
