2156779c7382c3cd084efda9e3b54b082e714870
Bo Chen <bochen1993cs@gmail.com>
2022-01-25 23:14:10 -0600

    Update main.py

--- main.py
+++ main.py
@@ -25,10 +25,10 @@ from models import load_state_dict_whatever, load_state_dict_all, load_state_dic
 from dataset import VideoDataset, FrameDataset
 
 # OPTION
-CODEC_NAME = 'SPVC64-N-P'
+CODEC_NAME = 'SPVC64-N-O'
 SAVE_DIR = f'backup/{CODEC_NAME}'
 loss_type = 'P'
-compression_level = 3 # 0,1,2,3
+compression_level = 0 # 0,1,2,3
 #RESUME_CODEC_PATH = f'{SAVE_DIR}/{CODEC_NAME}-{compression_level}{loss_type}_ckpt.pth'
 RESUME_CODEC_PATH = f'backup/SPVC64-N/SPVC64-N-{compression_level}{loss_type}_best.pth'
 LEARNING_RATE = 0.0001
@@ -74,7 +74,7 @@ best_codec_score = [1,0,0]
 if CODEC_NAME in ['x265', 'x264', 'RAW']:
     # nothing to load
     print("No need to load for ", CODEC_NAME)
-elif CODEC_NAME in ['SPVC64-N-P']:
+elif CODEC_NAME in []:
     # load what exists
     pretrained_model_path = f'backup/SPVC64-N/SPVC64-N-{compression_level}{loss_type}_best.pth'
     checkpoint = torch.load(pretrained_model_path,map_location=torch.device('cuda:'+str(device)))
