c87715c707ba1b13afc24d0d6a319f80fcdc206d
Bo Chen <bochen1993cs@gmail.com>
2022-03-08 08:51:39 -0600

    Update prune.py

--- prune.py
+++ prune.py
@@ -26,7 +26,7 @@ def load_checkpoint(model, filename):
     state_dict = checkpoint['state_dict']
     own_state = model.state_dict()
     for name, param in state_dict.items():
-        print(name,param.size())
+        print(name,param.size(),param.out_mask.sum())
         own_state[name].copy_(param)
     exit(0)
 
