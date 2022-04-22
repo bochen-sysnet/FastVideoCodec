77a31354e373308dc46233d0f3a42255e742e655
Bo Chen <bochen1993cs@gmail.com>
2022-04-22 01:11:49 -0500

    Update prune.py

--- prune.py
+++ prune.py
@@ -345,10 +345,11 @@ class FisherPruningHook():
                 sum_fisher = torch.sum(fisher*in_mask)
                 fisher /= sum_fisher
             # print(name,torch.sum(fisher),fisher)
-            fisher_list = np.concatenate((fisher_list,fisher.cpu().view(-1).numpy().astype(np.uint16)))
+            fisher_list = np.concatenate((fisher_list,fisher.cpu().view(-1).numpy().astype(np.int32)))
             info.update(
                 self.find_pruning_channel(module, fisher, in_mask, info))
         print(len(fisher_list))
+        plt.figure(1)
         sns.displot(fisher_list, kind='hist', aspect=1.2)
         plt.savefig('single.png')
         return info
@@ -375,9 +376,10 @@ class FisherPruningHook():
                 sum_fisher = torch.sum(fisher*in_mask)
                 fisher /= sum_fisher
             #print(group,self.groups[group][0].name,torch.sum(fisher),fisher)
-            fisher_list = np.concatenate((fisher_list,fisher.cpu().view(-1).numpy().astype(np.uint16)))
+            fisher_list = np.concatenate((fisher_list,fisher.cpu().view(-1).numpy().astype(np.int32)))
             info.update(self.find_pruning_channel(group, fisher, in_mask, info))
         print(len(fisher_list))
+        plt.figure(2)
         sns.displot(fisher_list, kind='hist', aspect=1.2)
         plt.savefig('group.png')
         module, channel = info['module'], info['channel']
