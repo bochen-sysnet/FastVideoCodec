7eabbc9dc3192c64aad622f56b9504c6f3423866
Bo Chen <bochen1993cs@gmail.com>
2022-04-22 01:09:58 -0500

    Update prune.py

--- prune.py
+++ prune.py
@@ -338,7 +338,7 @@ class FisherPruningHook():
                     out_rep = ancestor.out_rep if type(module).__name__ == 'Linear' else 1
                     delta_acts += self.acts[ancestor] / ancestor.out_channels * out_rep
                 #fisher /= (float(max(delta_acts, 1.)) / 1e6)
-                fisher /= float(self.acts[group])
+                fisher /= float(max(delta_acts, 1.))
             if self.fisher_norm:
                 # make sure the fisher info in the same group
                 # sum to 1, excluding pruned channels
