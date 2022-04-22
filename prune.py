41c0ed8dd7c8cf5d7a62cf2219ea6515e27079c9
Bo Chen <bochen1993cs@gmail.com>
2022-04-22 11:06:29 -0500

    Update prune.py

--- prune.py
+++ prune.py
@@ -189,8 +189,10 @@ class FisherPruningHook():
             # plot figure
             if itr % 1000 == 0:
                 plt.figure(1)
-                self.fisher_list[self.fisher_list==0] = 1e-50
-                sns.displot(np.log10(self.fisher_list), kind='hist', aspect=1.2)
+                self.fisher_list[self.fisher_list==0] = 1e20
+                self.fisher_list = np.log10(self.fisher_list)
+                self.fisher_list[self.fisher_list==20] = 0
+                sns.displot(self.fisher_list, kind='hist', aspect=1.2)
                 plt.savefig(f'fisher_distribution_{itr}.png')
         self.init_flops_acts()
         exit(0)
