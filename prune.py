f8c2b5d7cfdcdcb66786d88ebd1ec6c915d693c4
Bo Chen <bochen1993cs@gmail.com>
2022-04-23 00:37:43 -0500

    Update prune.py

--- prune.py
+++ prune.py
@@ -449,10 +449,9 @@ class FisherPruningHook():
             
     def set_target(self, target):
         for module, name in self.conv_names.items():
-            print(len(self.conv_inputs[module]))
             for inp in self.conv_inputs[module]:
                 print('?',module.name, target.size(), len(inp))
-                grad_feature = torch.autograd.grad(target, inp, create_graph=True)[0]
+                grad_feature = torch.autograd.grad(target, inp[0], create_graph=True)[0]
                 print(grad_feature.size())
 
     def save_input_forward_hook(self, module, inputs, outputs):
