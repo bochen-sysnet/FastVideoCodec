512c7d6470bacc006c5f87c0be8c0ba2afacf503
Bo Chen <bochen1993cs@gmail.com>
2022-04-29 14:07:55 -0500

    Update prune.py

--- prune.py
+++ prune.py
@@ -464,7 +464,7 @@ class FisherPruningHook():
         w_grad = w.grad
         w = w.detach()
         grad_adjust = fisher*(w_grad + w*w_grad*w_grad)
-        grad_adjust *= penalty
+        grad_adjust *= -penalty
         if hasattr(module, 'weight'):
             module.weight.grad += grad_adjust
         else:
