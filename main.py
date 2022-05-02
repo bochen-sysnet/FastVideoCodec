911633026a5c6b6d5c629373e580e099ed836123
Bo Chen <bochen1993cs@gmail.com>
2022-05-02 17:29:23 -0500

    Update main.py

--- main.py
+++ main.py
@@ -196,7 +196,7 @@ def train(epoch, model, train_dataset, optimizer, best_codec_score, test_dataset
             hook.use_mask = False
             com_data_no_mask, _, _, be_loss_no_mask, *_ = run_one_iteration(model, data)
             hook.use_mask = True
-            quality_penalty = 10.0*torch.log10(1/torch.mean(torch.pow(com_data_no_mask - com_data.detach(), 2)))
+            quality_penalty = torch.mean(torch.pow(com_data_no_mask - com_data.detach(), 2))
             bpp_penalty = be_loss.detach() - be_loss_no_mask # no mask should be close to with mask
             loss2 = computation_penalty + quality_penalty + bpp_penalty
             print(batch_idx,computation_penalty,quality_penalty,bpp_penalty)
