c928f2444d2f4b64aabe44da31aad4911d98b594
Bo Chen <bochen1993cs@gmail.com>
2022-05-02 11:57:04 -0500

    Update main.py

--- main.py
+++ main.py
@@ -189,7 +189,7 @@ def train(epoch, model, train_dataset, optimizer, best_codec_score, test_dataset
         # backward
         scaler.scale(loss).backward()
         
-        if hook.trained_mask and False:
+        if hook.trained_mask:
             # train iteratively since memory insufficient
             computation_penalty = hook.computation_penalty()
             hook.use_mask = False
