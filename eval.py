2f0dc324cec5cf2bd84a5e8ca3d04f55ec03cad2
Bo Chen <bochen1993cs@gmai.com>
2022-12-23 14:20:37 -0600

    split compress

--- eval.py
+++ eval.py
@@ -1161,8 +1161,8 @@ if __name__ == '__main__':
     parser.add_argument("--channels", type=int, default=128, help="Channels of SPVC")
     parser.add_argument('--fps', type=float, default=30., help='frame rate of sender')
     parser.add_argument('--target_rate', type=float, default=30., help='Target rate of receiver')
-    parser.add_argument("--width", type=int, default=256, help="Input width")
-    parser.add_argument("--height", type=int, default=256, help="Input height")
+    parser.add_argument("--width", type=int, default=1920, help="Input width")
+    parser.add_argument("--height", type=int, default=1280, help="Input height")
     args = parser.parse_args()
     
     # check gpu
