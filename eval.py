c4d5a3e411564ccf43f954f91e55d7a7d461b298
Bo Chen <bochen1993cs@gmai.com>
2022-12-23 14:08:00 -0600

    split compress

--- eval.py
+++ eval.py
@@ -118,9 +118,13 @@ class VideoDataset(Dataset):
                 # skip black frames
                 if np.sum(img) == 0:continue
                 img = Image.fromarray(img)
-                if self._frame_size is not None:
-                    img = img.resize(self._frame_size) 
-                self._clip.append(img)
+                # if self._frame_size is not None:
+                #     img = img.resize(self._frame_size) 
+                # self._clip.append(img)
+                self._clip.append(img.crop((0,0,self._frame_size[0]//2,self._frame_size[1]//2)))
+                self._clip.append(img.crop((self._frame_size[0]//2,0,self._frame_size[0],self._frame_size[1]//2)))
+                self._clip.append(img.crop((0,self._frame_size[1]//2,self._frame_size[0]//2,self._frame_size[1])))
+                self._clip.append(img.crop((self._frame_size[0]//2,self._frame_size[1]//2,self._frame_size[0],self._frame_size[1])))
             self._file_counter +=1
             self._dataset_nums.append(len(self._clip))
             self._frame_counter = 0
@@ -1161,8 +1165,8 @@ if __name__ == '__main__':
     parser.add_argument("--channels", type=int, default=128, help="Channels of SPVC")
     parser.add_argument('--fps', type=float, default=30., help='frame rate of sender')
     parser.add_argument('--target_rate', type=float, default=30., help='Target rate of receiver')
-    parser.add_argument("--width", type=int, default=256, help="Input width")
-    parser.add_argument("--height", type=int, default=256, help="Input height")
+    parser.add_argument("--width", type=int, default=1920, help="Input width")
+    parser.add_argument("--height", type=int, default=1280, help="Input height")
     args = parser.parse_args()
     
     # check gpu
