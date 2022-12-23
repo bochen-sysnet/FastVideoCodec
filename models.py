682e44bdb21d86d83747fa40eb066ff741abb8f7
Bo Chen <bochen1993cs@gmai.com>
2022-12-23 15:56:42 -0600

    split compress

--- models.py
+++ models.py
@@ -306,6 +306,7 @@ def parallel_compression(model, data, compressI=False):
             x_hat = torch.cat(x_hat_list,dim=0)
         elif 'LSVC' in model.name:
             B,_,H,W = data.size()
+            print(data.size())
             x_hat, x_mc, x_wp, rec_loss, warp_loss, mc_loss, bpp_res, bpp = model(data.detach())
             if model.stage == 'MC':
                 img_loss = mc_loss*model.r
