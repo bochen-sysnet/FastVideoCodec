aafdd0fcda515f6f635e8931ae26c6a971f4f006
Bo Chen <bochen1993cs@gmai.com>
2022-12-26 02:05:39 -0600

    train non attn linear from dvc

--- train.py
+++ train.py
@@ -78,7 +78,7 @@ elif CODEC_NAME in ['LSVC-L']:
     # load what exists
     pretrained_model_path = 'DVC/snapshot/512.model'#'backup/LSVC/LSVC-0P_best.pth'
     checkpoint = torch.load(pretrained_model_path,map_location=torch.device('cuda:'+str(device)))
-    best_codec_score = checkpoint['score']
+    best_codec_score = checkpoint['score'] if 'score' in checkpoint.keys() else 0
     load_state_dict_whatever(model, checkpoint['state_dict'])
     del checkpoint
     print("Load whatever exists for",CODEC_NAME,'from',pretrained_model_path,best_codec_score)
