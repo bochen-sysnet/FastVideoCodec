8829954ef751ac89027b5b08ca43dd7323dfa8a0
Bo Chen <bochen1993cs@gmai.com>
2022-12-28 14:43:44 -0600

    data parallel

--- models.py
+++ models.py
@@ -978,13 +978,13 @@ Backward_tensorGrid = {}
 
 def torch_warp(tensorInput, tensorFlow):
     device_id = tensorInput.device.index
-    print(device_id)
     if str(tensorFlow.size()) not in Backward_tensorGrid:
             tensorHorizontal = torch.linspace(-1.0, 1.0, tensorFlow.size(3)).view(1, 1, 1, tensorFlow.size(3)).expand(tensorFlow.size(0), -1, tensorFlow.size(2), -1)
             tensorVertical = torch.linspace(-1.0, 1.0, tensorFlow.size(2)).view(1, 1, tensorFlow.size(2), 1).expand(tensorFlow.size(0), -1, -1, tensorFlow.size(3))
             Backward_tensorGrid[str(tensorFlow.size())] = torch.cat([ tensorHorizontal, tensorVertical ], 1).to(device_id)
 
     tensorFlow = torch.cat([tensorFlow[:, 0:1, :, :] / ((tensorInput.size(3) - 1.0) / 2.0), tensorFlow[:, 1:2, :, :] / ((tensorInput.size(2) - 1.0) / 2.0) ], 1)
+    print(Backward_tensorGrid.device.index,tensorFlow.device.index)
 
     return torch.nn.functional.grid_sample(input=tensorInput, grid=(Backward_tensorGrid[str(tensorFlow.size())] + tensorFlow).permute(0, 2, 3, 1), mode='bilinear', padding_mode='border')
 
