43106351d5a1a0328df754cafee61f16c1f69662
Bo Chen <bochen1993cs@gmai.com>
2022-12-30 14:54:31 -0600

    test 1920 640

--- eval.py
+++ eval.py
@@ -235,7 +235,7 @@ def static_bench_x26x():
     
 def static_simulation_model(args, test_dataset):
     max_level = max(4,args.target_level)
-    for lvl in range(max_level+1):
+    for lvl in range(max_level):
         if args.Q_option != 'Slow' and lvl>0:continue
         model = LoadModel(args.task,compression_level=lvl,use_split=args.use_split)
         if args.use_cuda:
