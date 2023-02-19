b8766d2e9c835eaf1fa8d329dce253b432cfec5e
Bo Chen <bochen1993cs@gmai.com>
2023-02-19 10:04:17 -0600

    add SSF

--- models.py
+++ models.py
@@ -2172,29 +2172,41 @@ class ScaleSpaceFlow(nn.Module):
             def forward(self, y):
                 z = self.hyper_encoder(y)
                 # z_hat = quantize_ste(z)
-                # z_bits = self.entropy_bottleneck(z_hat)
-                z_hat, z_likelihoods = self.entropy_bottleneck(z)
-                z_bits = torch.sum(torch.clamp(-1.0 * torch.log(z_likelihoods + 1e-5) / math.log(2.0), 0, 50))
+                # z_hat, z_likelihoods = self.entropy_bottleneck(z)
+                # z_bits = torch.sum(torch.clamp(-1.0 * torch.log(z_likelihoods + 1e-5) / math.log(2.0), 0, 50))
+                z_bits = self.entropy_bottleneck(z_hat)
+                if self.training:
+                    half = float(0.5)
+                    noise = torch.empty_like(z).uniform_(-half, half)
+                    z_hat = z + noise
+                else:
+                    z_hat = torch.round(z)
 
                 scales = self.hyper_decoder_scale(z_hat)
                 means = self.hyper_decoder_mean(z_hat)
-                # y_bits = self.gaussian_conditional(y, scales, means)
-                _, y_likelihoods = self.gaussian_conditional(y, scales, means)
-                y_bits = torch.sum(torch.clamp(-1.0 * torch.log(y_likelihoods + 1e-5) / math.log(2.0), 0, 50))
-                y_hat = quantize_ste(y - means) + means
+                y_bits = self.gaussian_conditional(y, scales, means)
+                # _, y_likelihoods = self.gaussian_conditional(y, scales, means)
+                # y_bits = torch.sum(torch.clamp(-1.0 * torch.log(y_likelihoods + 1e-5) / math.log(2.0), 0, 50))
+                # y_hat = quantize_ste(y - means) + means
+                if self.training:
+                    half = float(0.5)
+                    noise = torch.empty_like(y).uniform_(-half, half)
+                    y_hat = y + noise
+                else:
+                    y_hat = torch.round(y)
                 return y_hat, z_bits + y_bits
 
-            # def gaussian_conditional(self,feature, sigma, mu):
-            #     sigma = sigma.clamp(1e-5, 1e10)
-            #     gaussian = torch.distributions.laplace.Laplace(mu, sigma)
-            #     probs = gaussian.cdf(feature + 0.5) - gaussian.cdf(feature - 0.5)
-            #     total_bits = torch.sum(torch.clamp(-1.0 * torch.log(probs + 1e-5) / math.log(2.0), 0, 50))
-            #     return total_bits
+            def gaussian_conditional(self,feature, sigma, mu):
+                sigma = sigma.clamp(1e-5, 1e10)
+                gaussian = torch.distributions.laplace.Laplace(mu, sigma)
+                probs = gaussian.cdf(feature + 0.5) - gaussian.cdf(feature - 0.5)
+                total_bits = torch.sum(torch.clamp(-1.0 * torch.log(probs + 1e-5) / math.log(2.0), 0, 50))
+                return total_bits
 
-            # def entropy_bottleneck(self,feature):
-            #     prob = self.bitEstimator(feature + 0.5) - self.bitEstimator(feature - 0.5)
-            #     total_bits = torch.sum(torch.clamp(-1.0 * torch.log(prob + 1e-5) / math.log(2.0), 0, 50))
-            #     return total_bits
+            def entropy_bottleneck(self,feature):
+                prob = self.bitEstimator(feature + 0.5) - self.bitEstimator(feature - 0.5)
+                total_bits = torch.sum(torch.clamp(-1.0 * torch.log(prob + 1e-5) / math.log(2.0), 0, 50))
+                return total_bits
 
         self.res_encoder = Encoder(3)
         self.res_decoder = Decoder(3, in_planes=384)
--- train.py
+++ train.py
@@ -25,11 +25,11 @@ from models import load_state_dict_whatever, load_state_dict_all, load_state_dic
 from dataset import VideoDataset, FrameDataset
 
 # OPTION
-CODEC_NAME = 'SSF2'
+CODEC_NAME = 'SSF'
 SAVE_DIR = f'backup/{CODEC_NAME}'
 loss_type = 'P'
 compression_level = 0 # 0,1,2,3
-RESUME_CODEC_PATH = f'backup/{CODEC_NAME}/{CODEC_NAME}-{compression_level}{loss_type}3_ckpt.pth'
+RESUME_CODEC_PATH = f'backup/{CODEC_NAME}/{CODEC_NAME}-{compression_level}{loss_type}_best.pth'
 LEARNING_RATE = 0.0001
 WEIGHT_DECAY = 5e-4
 BEGIN_EPOCH = 1
