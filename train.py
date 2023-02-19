3467844479fd921ad02c5c602b2a42282597962e
Bo Chen <bochen1993cs@gmai.com>
2023-02-19 13:06:31 -0600

    test RF

--- train.py
+++ train.py
@@ -25,7 +25,7 @@ from models import load_state_dict_whatever, load_state_dict_all, load_state_dic
 from dataset import VideoDataset, FrameDataset
 
 # OPTION
-CODEC_NAME = 'Base'
+CODEC_NAME = 'BaseRF'
 SAVE_DIR = f'backup/{CODEC_NAME}'
 loss_type = 'P'
 compression_level = 0 # 0,1,2,3
@@ -73,7 +73,7 @@ best_codec_score = [1,0,0]
 if CODEC_NAME in ['x265', 'x264', 'RAW']:
     # nothing to load
     print("No need to load for ", CODEC_NAME)
-elif CODEC_NAME in ['Base','BaseRNN','BaseDM','BaseRF','BaseMOD','SSF']:
+elif CODEC_NAME in ['Base','BaseRF','SSF']:
     # load what exists
     pretrained_model_path = f'DVC/snapshot/512.model'#f'backup/LSVC-A/LSVC-A-{compression_level}P_best.pth'
     checkpoint = torch.load(pretrained_model_path,map_location=torch.device('cuda:'+str(device)))
