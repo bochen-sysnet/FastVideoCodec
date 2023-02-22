df46a8b5654cecf0864b338399a8cb20c896bc5e
Bo Chen <bochen1993cs@gmai.com>
2023-02-22 02:52:43 -0600

    test base er

--- models.py
+++ models.py
@@ -236,7 +236,7 @@ def parallel_compression(model, data, compressI=False):
                     model(data[i:i+1],x_prev,priors)
                 x_prev = x_prev.detach()
                 if model.useER or model.useE2R:
-                    all_loss_list += [(model.r*mseloss + bpp + alpha * model.r*(mseloss_real - mseloss) + alpha * (bpp_real - bpp)).to(data.device)]
+                    all_loss_list += [(model.r*mseloss + bpp + alpha * model.r*(mseloss - mseloss_real) + alpha * (bpp - bpp_real)).to(data.device)]
                 else:
                     all_loss_list += [(model.r*mseloss + bpp).to(data.device)]
                 img_loss_list += [model.r*mseloss.to(data.device)]
@@ -246,8 +246,8 @@ def parallel_compression(model, data, compressI=False):
                 psnr_list += [10.0*torch.log(1/mseloss)/torch.log(torch.FloatTensor([10])).squeeze(0).to(data.device)]
                 # aux_loss_list += [bpp_real.to(data.device)]
                 # aux2_loss_list += [10.0*torch.log(1/mseloss_real)/torch.log(torch.FloatTensor([10])).squeeze(0).to(data.device)]
-                aux_loss_list += [model.r*(mseloss_real - mseloss).to(data.device)]
-                aux2_loss_list += [(bpp_real - bpp).to(data.device)]
+                aux_loss_list += [model.r*(mseloss - mseloss_real).to(data.device)]
+                aux2_loss_list += [(bpp - bpp_real).to(data.device)]
                 x_hat_list.append(x_prev)
             x_hat = torch.cat(x_hat_list,dim=0)
         elif model_name in ['DVC','RLVC','RLVC2']:
