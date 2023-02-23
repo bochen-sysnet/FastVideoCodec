b653a51d3806e18ba94de8eb5894eca38864b3f9
Bo Chen <bochen1993cs@gmai.com>
2023-02-23 15:04:35 -0600

    base er add teacher

--- models.py
+++ models.py
@@ -2009,9 +2009,9 @@ class Base(nn.Module):
                 if self.useER: 
                     quant_noise_mv = noise_level = torch.sigmoid(quant_noise_mv) - 0.5
                 elif self.useE2R:
-                    noise_level = 1#torch.sigmoid(quant_noise_mv)
+                    noise_level = torch.sigmoid(quant_noise_mv)
                     eps = torch.empty_like(noise_level).uniform_(-float(.5), float(.5))
-                    quant_noise_mv = (noise_level * eps)
+                    quant_noise_mv = (1 * eps)
                 else:
                     half = float(0.5)
                     quant_noise_mv = noise_level = torch.empty_like(mvfeature).uniform_(-half, half)
@@ -2060,9 +2060,9 @@ class Base(nn.Module):
                     quant_noise_feature = noise_level = torch.sigmoid(quant_noise_feature) - 0.5
                 elif self.useE2R:
                     quant_noise_feature = self.resErrNet((input_residual))
-                    noise_level = 1#torch.sigmoid(quant_noise_feature)
+                    noise_level = torch.sigmoid(quant_noise_feature)
                     eps = torch.empty_like(noise_level).uniform_(-float(.5), float(.5))
-                    quant_noise_feature = (noise_level * eps)
+                    quant_noise_feature = (1 * eps)
                 else:
                     half = float(0.5)
                     quant_noise_feature = noise_level = torch.empty_like(feature).uniform_(-half, half)
@@ -2085,9 +2085,9 @@ class Base(nn.Module):
                 quant_noise_z = noise_level = torch.sigmoid(quant_noise_z) - 0.5
             elif self.useE2R:
                 quant_noise_z = self.respriorErrNet((feature))
-                noise_level = 1#torch.sigmoid(quant_noise_z)
+                noise_level = torch.sigmoid(quant_noise_z)
                 eps = torch.empty_like(noise_level).uniform_(-float(.5), float(.5))
-                quant_noise_z = (noise_level * eps)
+                quant_noise_z = (1 * eps)
             else:
                 half = float(0.5)
                 quant_noise_z = noise_level = torch.empty_like(z).uniform_(-half, half)
