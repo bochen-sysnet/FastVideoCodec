ef48b335e767f0c7425a9f8e49ee6d49fb08e302
Bo Chen <bochen1993cs@gmai.com>
2023-02-23 15:50:48 -0600

    base er add teacher

--- train.py
+++ train.py
@@ -242,7 +242,7 @@ def train(epoch, model, train_dataset, optimizer, best_codec_score, test_dataset
             aux2_loss_module.reset() 
             I_module.reset()    
             
-        if batch_idx % 10000 == 0 and batch_idx>0:
+        if batch_idx % 10000 == 0:# and batch_idx>0:
             if True:
                 print('Testing at batch_idx %d' % (batch_idx))
                 score = test(epoch, model, test_dataset)
