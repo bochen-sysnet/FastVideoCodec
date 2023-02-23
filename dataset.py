89b991a302a896cf14113211537023499f08a3ae
Bo Chen <bochen1993cs@gmai.com>
2023-02-22 20:48:28 -0600

    test yuv

--- dataset.py
+++ dataset.py
@@ -109,8 +109,8 @@ class VideoDataset(Dataset):
                 # Capture frame-by-frame
                 ret, img = cap.read()
                 if ret != True:break
-                cv2.imwrite('../test.jpg',img)
-                exit(0)
+                # cv2.imwrite('../test.jpg',img)
+                # exit(0)
                 if np.sum(img) == 0:continue
                 self._total_frames+=1
             print(self._total_frames);self._total_frames = 0
