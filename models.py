6d979e31f84f3dcc8098c42effff7c3e663e7a89
Bo Chen <bochen1993cs@gmai.com>
2023-02-24 17:19:56 -0600

    gan er

--- models.py
+++ models.py
@@ -1852,10 +1852,7 @@ class BasicBlock(nn.Module):
 
     def forward(self, x):
         out = F.relu(self.bn1(self.conv1(x)))
-
         out = self.bn2(self.conv2(out))
-
-        out = self.expand_layer(out)
         out += self.shortcut(x)
         out = F.relu(out)
         return out
