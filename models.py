e80341fd75b685205ffd240f355d7f68858f4298
Bo Chen <bochen1993cs@gmai.com>
2023-02-23 18:17:28 -0600

    base er

--- models.py
+++ models.py
@@ -1973,14 +1973,14 @@ class Base(nn.Module):
             # self.mvErrNet = Analysis_mv_net(out_channels = out_channel_mv)
             # self.resErrNet = Analysis_net(out_channels = out_channel_M)
             # self.respriorErrNet = Analysis_prior_net(conv_channels = out_channel_N)
-            self.mvErrNet = CodecNet([(0,3,2,2,128),6,
-                                        (0,3,2,128,128),6,
-                                        (0,3,2,128,128),6,
-                                        (0,3,2,128,128)])
-            self.resErrNet = CodecNet([(0,3,2,3,128),6,
-                                        (0,3,2,128,128),6,
-                                        (0,3,2,128,128),6,
-                                        (0,3,2,128,96)])
+            self.mvErrNet = CodecNet([(0,5,2,2,128),6,
+                                        (0,5,2,128,128),6,
+                                        (0,5,2,128,128),6,
+                                        (0,5,2,128,128)])
+            self.resErrNet = CodecNet([(0,5,2,3,128),6,
+                                        (0,5,2,128,128),6,
+                                        (0,5,2,128,128),6,
+                                        (0,5,2,128,96)])
             self.respriorErrNet = CodecNet([(0,3,1,96,128),6,
                                         (0,5,2,128,128),6,
                                         (0,5,2,128,64)])
