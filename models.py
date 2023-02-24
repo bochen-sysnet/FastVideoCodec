b8458884a0f158d137335c05632988a2a37a6561
Bo Chen <bochen1993cs@gmai.com>
2023-02-24 01:37:49 -0600

    debug er

--- models.py
+++ models.py
@@ -2013,9 +2013,8 @@ class Base(nn.Module):
                     quant_noise_mv = self.mvErrNet(estmv)
             if self.training:
                 if self.useER:
-                    quant_noise_mv = torch.sigmoid(quant_noise_mv) - 0.5
-                    max_noise = torch.abs(quant_noise_mv).max()
-                    quant_noise_mv = quant_noise_mv * 0.5/max_noise
+                    quant_noise_mv = torch.sigmoid(quant_noise_mv) * torch.empty_like(quant_noise_mv).uniform_(-float(.5), float(.5))
+                    quant_noise_mv = quant_noise_mv * 0.5 / torch.abs(quant_noise_mv).max()
                 elif self.useE2R:
                     noise_level = torch.sigmoid(quant_noise_mv)
                     eps = torch.empty_like(noise_level).uniform_(-float(.5), float(.5))
@@ -2065,9 +2064,8 @@ class Base(nn.Module):
                 if self.useER: 
                     # predict STE behavior
                     quant_noise_feature = self.resErrNet((input_residual))
-                    quant_noise_feature = torch.sigmoid(quant_noise_feature) - 0.5
-                    max_noise = torch.abs(quant_noise_feature).max()
-                    quant_noise_feature = quant_noise_feature * 0.5/max_noise
+                    quant_noise_feature = torch.sigmoid(quant_noise_feature) * torch.empty_like(quant_noise_feature).uniform_(-float(.5), float(.5))
+                    quant_noise_feature = quant_noise_feature * 0.5 / torch.abs(quant_noise_feature).max()
                 elif self.useE2R:
                     quant_noise_feature = self.resErrNet((input_residual))
                     noise_level = torch.sigmoid(quant_noise_feature)
@@ -2091,9 +2089,8 @@ class Base(nn.Module):
         if self.training:
             if self.useER: 
                 quant_noise_z = self.respriorErrNet((feature))
-                quant_noise_z = torch.sigmoid(quant_noise_z) - 0.5
-                max_noise = torch.abs(quant_noise_z).max()
-                quant_noise_z = quant_noise_z * 0.5/max_noise
+                quant_noise_z = torch.sigmoid(quant_noise_z) * torch.empty_like(quant_noise_z).uniform_(-float(.5), float(.5))
+                quant_noise_z = quant_noise_z * 0.5 / torch.abs(quant_noise_z).max()
             elif self.useE2R:
                 quant_noise_z = self.respriorErrNet((feature))
                 noise_level = torch.sigmoid(quant_noise_z)
