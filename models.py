5a61178bad8b907df9d1f18dc3f7be4b0c829c17
Bo Chen <bochen1993cs@gmai.com>
2023-02-26 17:33:22 -0600

    er 0

--- models.py
+++ models.py
@@ -2029,20 +2029,20 @@ class Base(nn.Module):
             #                         (0,3,1,128,128),3,
             #                         (0,3,1,128,64),7])
             ch1,ch2,ch3 = 192,128,128
-            kernel_size = 3
-            num_blocks = 4
+            kernel_size = 5
+            num_blocks = 2
             self.residualER = True
-            self.additiveER = False
+            self.additiveER = True
             print(kernel_size,num_blocks,self.residualER,self.additiveER)
             # ER1 add, 4, attach, 
-            self.mvGenNet = nn.ModuleList([CodecNet([(0,kernel_size,1,128,ch1),3,(0,kernel_size,1,ch1,128),3]) for _ in range(num_blocks)]) 
-            self.resGenNet = nn.ModuleList([CodecNet([(0,kernel_size,1,96,ch2),3,(0,kernel_size,1,ch2,96),3]) for _ in range(num_blocks)])
-            self.respriorGenNet = nn.ModuleList([CodecNet([(0,kernel_size,1,64,ch3),3,(0,kernel_size,1,ch3,64),3]) for _ in range(num_blocks)])
-            # self.mvGenNet = nn.ModuleList([CodecNet([(0,kernel_size,1,128,ch1),3,(0,kernel_size,1,ch1,ch1),3,(0,kernel_size,1,ch1,ch1),3,(0,kernel_size,1,ch1,128),3]) for _ in range(num_blocks)]) 
-            # self.resGenNet = nn.ModuleList([CodecNet([(0,kernel_size,1,96,ch2),3,(0,kernel_size,1,ch2,ch2),3,(0,kernel_size,1,ch2,ch2),3,(0,kernel_size,1,ch2,96),3]) for _ in range(num_blocks)])
-            # self.respriorGenNet = nn.ModuleList([CodecNet([(0,kernel_size,1,64,ch3),3,(0,kernel_size,1,ch3,ch3),3,(0,kernel_size,1,ch3,ch3),3,(0,kernel_size,1,ch3,64),3]) for _ in range(num_blocks)])
+            # self.mvGenNet = nn.ModuleList([CodecNet([(0,kernel_size,1,128,ch1),3,(0,kernel_size,1,ch1,128),3]) for _ in range(num_blocks)]) 
+            # self.resGenNet = nn.ModuleList([CodecNet([(0,kernel_size,1,96,ch2),3,(0,kernel_size,1,ch2,96),3]) for _ in range(num_blocks)])
+            # self.respriorGenNet = nn.ModuleList([CodecNet([(0,kernel_size,1,64,ch3),3,(0,kernel_size,1,ch3,64),3]) for _ in range(num_blocks)])
+            self.mvGenNet = nn.ModuleList([CodecNet([(0,kernel_size,1,128,ch1),3,(0,kernel_size,1,ch1,ch1),3,(0,kernel_size,1,ch1,ch1),3,(0,kernel_size,1,ch1,128),3]) for _ in range(num_blocks)]) 
+            self.resGenNet = nn.ModuleList([CodecNet([(0,kernel_size,1,96,ch2),3,(0,kernel_size,1,ch2,ch2),3,(0,kernel_size,1,ch2,ch2),3,(0,kernel_size,1,ch2,96),3]) for _ in range(num_blocks)])
+            self.respriorGenNet = nn.ModuleList([CodecNet([(0,kernel_size,1,64,ch3),3,(0,kernel_size,1,ch3,ch3),3,(0,kernel_size,1,ch3,ch3),3,(0,kernel_size,1,ch3,64),3]) for _ in range(num_blocks)])
             # ER2 2, add
-            # ER3 4, single
+            # ER3 2, add
             # ER4 2, single
         self.bitEstimator_z = BitEstimator(out_channel_N)
         self.warp_weight = 0
