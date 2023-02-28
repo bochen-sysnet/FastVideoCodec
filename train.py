0d1fa7e0238f797addb5efc9bdd4ecac59d580ae
Bo Chen <bochen1993cs@gmai.com>
2023-02-28 14:50:50 -0600

    base debug

--- train.py
+++ train.py
@@ -312,7 +312,6 @@ def test(epoch, model, test_dataset):
             # aggregate loss
             ba_loss = torch.stack(bpp_act_list,dim=0).mean(dim=0)
             psnr = torch.stack(psnr_list,dim=0).mean(dim=0)
-            all_psnr_list += torch.stack(psnr_list,dim=0).tolist()
             
             # record loss
             ba_loss_module.update(ba_loss.cpu().data.item(), l)
