9b91800afdf416233d7d70ab33a2a01112bca4df
Bo Chen <bochen1993cs@gmai.com>
2023-03-06 21:18:00 -0600

    evolve

--- train.py
+++ train.py
@@ -378,15 +378,17 @@ if args.evaluate:
                 cur_loss = test(0, model, test_dataset, level, True, optimizer)
                 if cur_loss < min_loss:
                     min_loss = cur_loss
+                    best_state_dict = model.state_dict()
                 else:
                     converge_count += 1
-                if converge_count == 5:
-                    if shrink_count < 1:
+                if converge_count == 3:
+                    if shrink_count < 2:
                         shrink_learning_rate(optimizer)
                         converge_count = 0
                         shrink_count += 1
                     else:
                         break
+            load_state_dict_all(model, best_state_dict)
         score = test(0, model, test_dataset, level, False)
         if model.name not in ['ELFVC-L']:break
     exit(0)
