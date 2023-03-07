991f2583c02edd52dd152926ce76bb8dc64f08f9
Bo Chen <bochen1993cs@gmai.com>
2023-03-06 22:59:46 -0600

    evolve

--- train.py
+++ train.py
@@ -252,7 +252,7 @@ def test(epoch, model, test_dataset, level=0, evolve=False, optimizer=None):
     # finetune option
     if evolve:
         scaler = torch.cuda.amp.GradScaler(enabled=True)
-        model.train()
+        # model.train()
     img_loss_module = AverageMeter()
     ba_loss_module = AverageMeter()
     psnr_module = AverageMeter()
@@ -376,20 +376,20 @@ if args.evaluate:
             optimizer = torch.optim.Adam([{'params': parameters}], lr=LEARNING_RATE, weight_decay=WEIGHT_DECAY)
             for _ in range(20):
                 cur_loss = test(0, model, test_dataset, level, True, optimizer)
-                if cur_loss < min_loss:
-                    min_loss = cur_loss
-                    best_state_dict = model.state_dict()
-                    converge_count = 0
-                else:
-                    converge_count += 1
-                if converge_count == 5:
-                    if shrink_count < 2:
-                        shrink_learning_rate(optimizer)
-                        converge_count = 0
-                        shrink_count += 1
-                    else:
-                        break
-            load_state_dict_all(model, best_state_dict)
+            #     if cur_loss < min_loss:
+            #         min_loss = cur_loss
+            #         best_state_dict = model.state_dict()
+            #         converge_count = 0
+            #     else:
+            #         converge_count += 1
+            #     if converge_count == 5:
+            #         if shrink_count < 2:
+            #             shrink_learning_rate(optimizer)
+            #             converge_count = 0
+            #             shrink_count += 1
+            #         else:
+            #             break
+            # load_state_dict_all(model, best_state_dict)
         score = test(0, model, test_dataset, level, False)
         if model.name not in ['ELFVC-L']:break
     exit(0)
