cf266234046414165758376ad50a2a92b44d211f
Bo Chen <bochen1993cs@gmai.com>
2023-03-22 08:31:13 -0500

    elfvc ec er no round input inject middle

--- models.py
+++ models.py
@@ -1858,8 +1858,12 @@ class ELFVC(ScaleSpaceFlow):
                 elif pred_nc and side_channel_nc:
                     # self.y_predictor = CodecNet([(0,kernel_size,1,planes * 2,ch2),act_func,(0,kernel_size,1,ch2,ch2),act_func,(0,kernel_size,1,ch2,ch2),act_func,(0,kernel_size,1,ch2,planes),])
                     # self.channel_norm = ChannelNorm(planes * 2)
-                    self.y_predictor = CodecNet([(0,kernel_size,1,planes + 3,ch2),act_func,(0,kernel_size,1,ch2,ch2),act_func,(0,kernel_size,1,ch2,ch2),act_func,(0,kernel_size,1,ch2,planes),])
-                    self.channel_norm = ChannelNorm(planes + 3)
+                    # self.y_predictor = CodecNet([(0,kernel_size,1,planes + 3,ch2),act_func,(0,kernel_size,1,ch2,ch2),act_func,(0,kernel_size,1,ch2,ch2),act_func,(0,kernel_size,1,ch2,planes),])
+                    # self.channel_norm = ChannelNorm(planes + 3)
+                    self.y_predictor1 = CodecNet([(0,kernel_size,1,planes * 2,ch2),act_func,(0,kernel_size,1,ch2,ch2),])
+                    self.y_predictor2 = HyperDecoder(planes, mid_planes, planes)
+                    self.y_predictor = CodecNet([(0,kernel_size,1,planes * 2,ch2),act_func,(0,kernel_size,1,ch2,ch2),])
+                    self.channel_norm = ChannelNorm(planes * 2)
                 elif not pred_nc and side_channel_nc:
                     self.y_predictor = HyperDecoder(planes, mid_planes, planes)
                 else:
@@ -1905,8 +1909,11 @@ class ELFVC(ScaleSpaceFlow):
                     # for example, use predictor to down sample to the dimension of z_hat, concat and upsample
                     round_y = torch.round(y - means)
                     # side_info = F.interpolate(input=z_hat, scale_factor=8, mode='bilinear', align_corners=True)
-                    side_info = z_hat.view(-1,3,16,16)
-                    all_info = torch.cat((round_y, side_info), dim=1)
+                    # side_info = z_hat.view(-1,3,16,16)
+                    # all_info = torch.cat((round_y, side_info), dim=1)
+                    info1 = self.y_predictor1(round_y)
+                    info2 = self.y_predictor2(z_hat)
+                    all_info = torch.cat((info1, info2), dim=1)
                     all_info = self.channel_norm(all_info)
                     pred_y = self.y_predictor(all_info) + round_y 
                     pred_err_y = pred_y - (y - means).detach()
