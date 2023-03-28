2653c6110709ee9650326ec1aab54533d4e47122
Bo Chen <bochen1993cs@gmai.com>
2023-03-28 15:27:12 -0500

    elfvc batch

--- models.py
+++ models.py
@@ -166,8 +166,7 @@ def parallel_compression(args,model, data, compressI=False, level=0):
             psnr_list += [psnr.to(data.device)]
     else:
         for i in range(data.size(0)):
-            x_hat, bpp_i, psnr_i = I_compression(data[i,0:1], I_level, model_name=name)
-            data[i,0:1] = x_hat
+            data[i,0:1], _,_ = I_compression(data[i,0:1], I_level, model_name=name)
     
     # P compression, not including I frame
     if data.size(0) > 1: 
