82e55c6ade26cb7e688d040c14955b2680ac1220
Bo Chen <bochen1993cs@gmai.com>
2023-03-28 16:17:24 -0500

    elfvc batch 1

--- models.py
+++ models.py
@@ -308,7 +308,7 @@ def parallel_compression(args,model, data, compressI=False, level=0):
 
     # aggregate loss
     loss = torch.stack(all_loss_list,dim=0).mean(dim=0) if all_loss_list else 0
-    print(bpp_list.tolist(),img_loss_list.tolist())
+    print(bpp_list,img_loss_list)
     be_loss = torch.stack(bpp_list,dim=0).mean(dim=0).cpu().data.item()
     be_res_loss = torch.stack(bppres_list,dim=0).mean(dim=0).cpu().data.item() if bppres_list else 0
     img_loss = torch.stack(img_loss_list,dim=0).mean(dim=0).cpu().data.item() if all_loss_list else 0
