8824a7aa8270daec0b91f7cf5894959c8cd5a30a
Bo Chen <bochen1993cs@gmai.com>
2023-04-01 15:34:21 -0500

    elfvc attach mse loss

--- train.py
+++ train.py
@@ -208,7 +208,6 @@ def train(epoch, model, train_dataset, best_codec_score, test_dataset):
             f"I:{img_loss_module.val:.2f} ({img_loss_module.avg:.2f}). "
             f"B:{be_loss_module.val:.2f} ({be_loss_module.avg:.2f}). "
             f"P:{psnr_module.val:.2f} ({psnr_module.avg:.2f}). "
-            f"R:{be_res_loss_module.val:.2f} ({be_res_loss_module.avg:.2f}). "
             f"PE:{aux_loss_module.val:.4f} ({aux_loss_module.avg:.4f}). "
             f"QE:{aux2_loss_module.val:.4f} ({aux2_loss_module.avg:.4f}). ")
             
