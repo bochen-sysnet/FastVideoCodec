e145a83d5599b87a826b0d12ea7703a1c7a60e3d
Bo Chen <bochen1993cs@gmai.com>
2023-04-21 08:06:02 -0500

    v2

--- models.py
+++ models.py
@@ -1911,7 +1911,7 @@ class ELFVC(ScaleSpaceFlow):
         self.compression_level = compression_level
         self.loss_type = loss_type
         init_training_params(self)
-        self.spstage = 1
+        self.spstage = 2
         self.alpha = 1
         motion_sp = self.spstage >= 1
         res_sp = self.spstage >= 2
@@ -1935,7 +1935,7 @@ class ELFVC(ScaleSpaceFlow):
         # warmup predictor fast
         # train normaly the flow ae
         # train normaly the res ae
-        # lr=1e-5; alpha=100; stage=1; 
+        # lr=1e-5; alpha=100; stage=1; 5 epochs
         # alpha=1
         # lr=1e-6
         # lr=1e-5;stage=2
