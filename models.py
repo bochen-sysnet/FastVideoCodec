0ee31202a079c5d10d2ea8e8c5faefaa181fc035
Bo Chen <bochen1993cs@gmai.com>
2023-05-18 14:34:45 -0500

    test ssf on multiview dataset

--- models.py
+++ models.py
@@ -156,6 +156,7 @@ def compress_whole_video(name, raw_clip, Q, width=256,height=256, GOP=16):
             for g in range(raw_clip.size(0)):
                 Y1_raw = raw_clip[g][v]
                 Y1_com = clip[i].unsqueeze(0)
+                print(Y1_raw.size(),Y1_com.size())
                 psnr_list += [PSNR(Y1_raw, Y1_com)]
                 msssim_list += [MSSSIM(Y1_raw, Y1_com)]
                 bpp_act_list += torch.FloatTensor([bpp])
