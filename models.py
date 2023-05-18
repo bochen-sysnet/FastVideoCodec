01d72be21a56ab3d459aabbf048e619a40eddc24
Bo Chen <bochen1993cs@gmai.com>
2023-05-18 11:17:47 -0500

    test ssf on multiview dataset

--- models.py
+++ models.py
@@ -2102,23 +2102,28 @@ class MCVC(ScaleSpaceFlow):
                     deconv(mid_planes, out_planes, kernel_size=5, stride=2),
                     nn.Sequential() if not cross_correlation else Residual(PreNorm(out_planes, Attention(out_planes))),
                 )
-        class HyperDecoderWithQReLU(nn.Sequential):
+        class HyperDecoderWithQReLU(nn.Module):
             def __init__(
                 self, in_planes: int = 192, mid_planes: int = 192, out_planes: int = 192
             ):
-                from compressai.layers import QReLU
+                super().__init__()
+
                 def qrelu(input, bit_depth=8, beta=100):
                     return QReLU.apply(input, bit_depth, beta)
-                super().__init__(
-                    deconv(in_planes, mid_planes, kernel_size=5, stride=2),
-                    qrelu,
-                    deconv(mid_planes, mid_planes, kernel_size=5, stride=2),
-                    qrelu,
-                    nn.Sequential() if not cross_correlation else Residual(PreNorm(mid_planes, Attention(mid_planes))),
-                    deconv(mid_planes, out_planes, kernel_size=5, stride=2),
-                    qrelu,
-                    nn.Sequential() if not cross_correlation else Residual(PreNorm(out_planes, Attention(out_planes))),
-                    )
+
+                self.deconv1 = deconv(in_planes, mid_planes, kernel_size=5, stride=2)
+                self.qrelu1 = qrelu
+                self.deconv2 = deconv(mid_planes, mid_planes, kernel_size=5, stride=2)
+                self.qrelu2 = qrelu
+                self.deconv3 = deconv(mid_planes, out_planes, kernel_size=5, stride=2)
+                self.qrelu3 = qrelu
+
+            def forward(self, x):
+                x = self.qrelu1(self.deconv1(x))
+                x = self.qrelu2(self.deconv2(x))
+                x = self.qrelu3(self.deconv3(x))
+
+                return x
         # can condition on prior latents of all other frames
         class Hyperprior(CompressionModel):
             def __init__(self, planes: int = 192, mid_planes: int = 192):
