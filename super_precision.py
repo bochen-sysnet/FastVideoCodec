4dfcb3c515f3c407535907b752800e7a3aeac22f
Bo Chen <bochen1993cs@gmai.com>
2023-05-24 12:54:32 -0500

    mcvc test

--- super_precision.py
+++ super_precision.py
@@ -120,6 +120,8 @@ class Attention(nn.Module):
             out = rearrange(out, 'b h (x y) d -> b (h d) x y', x = h, y = w)
         else:
             out = rearrange(out, '1 h (b x y) d -> b (h d) x y', x = h, y = w)
+            print(out.size())
+            exit(0)
         return self.to_out(out)
 
 # model
