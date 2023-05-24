cf90df69f9b40530c1b4da336daf14d0656e3761
Bo Chen <bochen1993cs@gmai.com>
2023-05-24 03:18:04 -0500

    mcvc a2

--- models.py
+++ models.py
@@ -2093,8 +2093,8 @@ class MCVC(ScaleSpaceFlow):
                         conv(mid_planes, mid_planes, kernel_size=5, stride=2),
                         nn.ReLU(inplace=True),
                         conv(mid_planes, out_planes, kernel_size=5, stride=2),
-                        # Residual(PreNorm(out_planes, Attention(out_planes, spatial=False))),
-                        # Residual(PreNorm(out_planes, Attention(out_planes, spatial=True))),
+                        Residual(PreNorm(out_planes, Attention(out_planes, spatial=False))),
+                        Residual(PreNorm(out_planes, Attention(out_planes, spatial=True))),
                     )
         class Decoder(nn.Sequential):
             def __init__(
@@ -2112,8 +2112,8 @@ class MCVC(ScaleSpaceFlow):
                     )
                 else:
                     super().__init__(
-                        # Residual(PreNorm(in_planes, Attention(in_planes, spatial=False))),
-                        # Residual(PreNorm(in_planes, Attention(in_planes, spatial=True))),
+                        Residual(PreNorm(in_planes, Attention(in_planes, spatial=False))),
+                        Residual(PreNorm(in_planes, Attention(in_planes, spatial=True))),
                         deconv(in_planes, mid_planes, kernel_size=5, stride=2),
                         nn.ReLU(inplace=True),
                         deconv(mid_planes, mid_planes, kernel_size=5, stride=2),
@@ -2140,8 +2140,8 @@ class MCVC(ScaleSpaceFlow):
                         nn.ReLU(inplace=True),
                         conv(mid_planes, mid_planes, kernel_size=5, stride=2),
                         nn.ReLU(inplace=True),
-                        Residual(PreNorm(mid_planes, Attention(mid_planes, spatial=False))),
-                        Residual(PreNorm(mid_planes, Attention(mid_planes, spatial=True))),
+                        # Residual(PreNorm(mid_planes, Attention(mid_planes, spatial=False))),
+                        # Residual(PreNorm(mid_planes, Attention(mid_planes, spatial=True))),
                         conv(mid_planes, out_planes, kernel_size=5, stride=2),
                         Residual(PreNorm(out_planes, Attention(out_planes, spatial=False))),
                         Residual(PreNorm(out_planes, Attention(out_planes, spatial=True))),
@@ -2164,8 +2164,8 @@ class MCVC(ScaleSpaceFlow):
                         Residual(PreNorm(in_planes, Attention(in_planes, spatial=True))),
                         deconv(in_planes, mid_planes, kernel_size=5, stride=2),
                         nn.ReLU(inplace=True),
-                        Residual(PreNorm(mid_planes, Attention(mid_planes, spatial=False))),
-                        Residual(PreNorm(mid_planes, Attention(mid_planes, spatial=True))),
+                        # Residual(PreNorm(mid_planes, Attention(mid_planes, spatial=False))),
+                        # Residual(PreNorm(mid_planes, Attention(mid_planes, spatial=True))),
                         deconv(mid_planes, mid_planes, kernel_size=5, stride=2),
                         nn.ReLU(inplace=True),
                         deconv(mid_planes, out_planes, kernel_size=5, stride=2),
@@ -2189,17 +2189,17 @@ class MCVC(ScaleSpaceFlow):
                 if cross_correlation:
                     self.attn1 = Residual(PreNorm(in_planes, Attention(in_planes, spatial=False)))
                     self.attn2 = Residual(PreNorm(in_planes, Attention(in_planes, spatial=True)))
-                    self.attn3 = Residual(PreNorm(mid_planes, Attention(mid_planes, spatial=False)))
-                    self.attn4 = Residual(PreNorm(mid_planes, Attention(mid_planes, spatial=True)))
+                #     self.attn3 = Residual(PreNorm(mid_planes, Attention(mid_planes, spatial=False)))
+                #     self.attn4 = Residual(PreNorm(mid_planes, Attention(mid_planes, spatial=True)))
 
             def forward(self, x):
                 if cross_correlation:
                     x = self.attn1(x)
                     x = self.attn2(x)
                 x = self.qrelu1(self.deconv1(x))
-                if cross_correlation:
-                    x = self.attn3(x)
-                    x = self.attn4(x)
+                # if cross_correlation:
+                #     x = self.attn3(x)
+                #     x = self.attn4(x)
                 x = self.qrelu2(self.deconv2(x))
                 x = self.qrelu3(self.deconv3(x))
 
