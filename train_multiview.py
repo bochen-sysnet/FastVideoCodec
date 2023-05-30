5fca855f70d92787c8be2fb8d7d34cfd1da6f26a
Bo Chen <bochen1993cs@gmai.com>
2023-05-30 18:23:59 -0500

    test two camera with attention

--- train_multiview.py
+++ train_multiview.py
@@ -164,8 +164,7 @@ def train(epoch, model, train_dataset, best_codec_score, test_dataset):
     train_iter = tqdm(train_loader)
     for batch_idx,data in enumerate(train_iter):
         b,g,v,c,h,w = data.size()
-        print( data.permute(1,0,2,3,4,5).size())
-        data = data.permute(1,0,2,3,4,5).view(g,b*v,c,h,w).cuda(device)
+        data = data.permute(1,0,2,3,4,5).reshape(g,b*v,c,h,w).cuda(device)
         
         # run model
         out_dec = model(data)
