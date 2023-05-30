f937d89720028c98b09ee2c6909b7e473e294423
Bo Chen <bochen1993cs@gmai.com>
2023-05-30 18:03:12 -0500

    test two camera with attention

--- train_multiview.py
+++ train_multiview.py
@@ -255,11 +255,11 @@ translate_x = 0.1  # Specify the horizontal translation in pixels
 translate_y = 0.2  # Specify the vertical translation in pixels
 
 # Define the transformations
-# view_transforms += [transforms.Compose([
-#     transforms.RandomRotation(angle),
-#     transforms.RandomAffine(0, translate=(translate_x, translate_y)),
-#     transforms.ToTensor()
-# ])]
+view_transforms += [transforms.Compose([
+    transforms.RandomRotation(angle),
+    transforms.RandomAffine(0, translate=(translate_x, translate_y)),
+    transforms.ToTensor()
+])]
 train_dataset = FrameDataset('../dataset/vimeo', frame_size=256, view_transforms=view_transforms) 
 test_dataset = SynVideoDataset(f'../dataset/{args.dataset}', (args.height, args.width), args.max_files, view_transforms=view_transforms)
 if args.evaluate:
