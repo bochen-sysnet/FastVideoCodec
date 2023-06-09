1c3e4f2b346b917b75c2b611adae5ee39c8f7a91
Bo Chen <bochen1993cs@gmai.com>
2023-06-09 12:39:05 -0500

    resilience of single view loss

--- models.py
+++ models.py
@@ -2271,8 +2271,8 @@ class MCVC(ScaleSpaceFlow):
     def forward_keyframe(self, x, mask=None):
         y = self.img_encoder(x)
         y_hat, likelihoods = self.img_hyperprior(y)
-        # if mask is not None:
-        #     y_hat[mask] *= 0.0
+        if mask is not None:
+            y_hat[mask] *= 0.0
         x_hat = self.img_decoder(y_hat)
         return x_hat, {"keyframe": likelihoods}
 
@@ -2307,8 +2307,8 @@ class MCVC(ScaleSpaceFlow):
         else:
             # should fix encoder for resilience
             # Set the selected batches to zero
-            # y_motion_hat[mask] *= 0.0
-            # y_res_hat[mask] *= 0.0
+            y_motion_hat[mask] *= 0.0
+            y_res_hat[mask] *= 0.0
 
             # motion
             masked_motion_info = self.motion_decoder(y_motion_hat)
