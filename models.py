03d72e5608ed3dea7c658c7a451b5e8cd971ac1e
Bo Chen <bochen1993cs@gmai.com>
2023-06-13 14:24:04 -0500

    force resilience

--- models.py
+++ models.py
@@ -2097,7 +2097,7 @@ def sample_mask_for_resilience(tensor, num_views, max_resilience, failure_probab
     
     # Sample m elements from the original list
     mask = random.sample(original_list, num_views - resilience)
-    print(mask,test_resilience)
+    mask = [3]
 
     # Sort both lists
     mask.sort()
