be662333c39c996d8b8e5eb4812c0b698e0e813f
Bo Chen <bochen1993cs@gmai.com>
2023-06-14 02:10:34 -0500

    test no attn

--- super_precision.py
+++ super_precision.py
@@ -105,7 +105,7 @@ class Attention(nn.Module):
 
     def forward(self, x):
         b, c, h, w = x.shape
-        num_views = b//2 if self.training else 1
+        num_views = 1#b//2 if self.training else 1
         qkv = self.to_qkv(x).chunk(3, dim = 1)
         if self.type == 0:
             q, k, v = map(lambda t: rearrange(t, 'b (h c) x y -> b h c (x y)', h = self.heads), qkv)
