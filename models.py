8b7af2c44f8e76d0e067c5fb92b41947512e64cd
Bo Chen <bochen1993cs@gmai.com>
2023-06-14 21:12:07 -0500

    test cat1

--- models.py
+++ models.py
@@ -2102,6 +2102,7 @@ def sample_mask_for_resilience(tensor, num_views, max_resilience, failure_probab
 
     # Sort both lists
     mask.sort()
+    print(mask)
 
     # replicate with batch
     batched_mask = []
