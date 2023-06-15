38c2e0555c231636a18ed1aee3771ead7f1b2eb5
Bo Chen <bochen1993cs@gmai.com>
2023-06-14 21:17:17 -0500

    test cat1

--- train_multiview.py
+++ train_multiview.py
@@ -244,7 +244,7 @@ def train(epoch, model, train_dataset, best_codec_score):
             optimizer.zero_grad()
 
         # add metrics
-        resi = np.round(train_dataset.num_views * (1 - completeness))
+        resi = int(np.round(train_dataset.num_views * (1 - completeness)))
         print(train_dataset.num_views, completeness,resi)
         psnr_vs_resilience[resi].update(psnr.cpu().data.item())
         bpp_vs_resilience[resi].update(bpp.cpu().data.item())
