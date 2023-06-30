ad8d7ef450a265240e9ea5d1e5d0f9a93b9d8224
Bo Chen <bochen1993cs@gmai.com>
2023-06-29 19:54:27 -0500

    train multi category from cat 0 level 1

--- train_multiview.py
+++ train_multiview.py
@@ -288,7 +288,8 @@ for category_id in range(5):
     train_dataset = MultiViewVideoDataset('../dataset/multicamera/',split='train',transform=shared_transforms,category_id=category_id,num_views=args.num_views)
     test_dataset = MultiViewVideoDataset('../dataset/multicamera/',split='test',transform=shared_transforms,category_id=category_id,num_views=args.num_views)
 
-    for compression_level in range(4):
+    start = 0 if category_id>0 else 1
+    for compression_level in range(start,4):
         model, optimizer, best_codec_score = get_model_n_optimizer_n_score_from_level(compression_level, category_id)
 
         cvg_cnt = 0
