9d8ab2a6eb07bc4c6c204d7834c7fe53155c71cd
Bo Chen <bochen1993cs@gmai.com>
2023-07-02 02:56:22 -0500

    test

--- models.py
+++ models.py
@@ -82,7 +82,7 @@ def compress_whole_video(name, raw_clip, Q, width=256,height=256, GOP=16, frame_
     output_filename = f'tmp/videostreams/{name}{frame_comb}.mp4'
     if frame_comb == 1:
         width *= raw_clip.size(1)
-    if frame_comb in [0,2]:
+    if frame_comb in [0]:
         GOP *= raw_clip.size(1)
     if name == 'x265-veryfast':
         cmd = f'/usr/bin/ffmpeg -y -s {width}x{height} -pixel_format bgr24 -f rawvideo -r {fps} -i pipe: -vcodec libx265 -pix_fmt yuv420p -preset veryfast -tune zerolatency -x265-params "crf={Q}:keyint={GOP}:verbose=1" {output_filename}'
@@ -129,10 +129,11 @@ def compress_whole_video(name, raw_clip, Q, width=256,height=256, GOP=16, frame_
                     process.stdin.write(np.array(img).tobytes())
             clip_size = raw_clip.size(0)*raw_clip.size(1)
         elif frame_comb == 3:
-            for g in range(raw_clip.size(0)):
-                img = to_pil(raw_clip[g][0])
-                process.stdin.write(np.array(img).tobytes())
-            clip_size = raw_clip.size(0)
+            for v in range(raw_clip.size(1)):
+                for g in range(raw_clip.size(0)):
+                    img = to_pil(raw_clip[g][v])
+                    process.stdin.write(np.array(img).tobytes())
+            clip_size = raw_clip.size(0)*raw_clip.size(1)
         else:
             print('Undefined frame comb:',frame_comb)
             exit(0)
@@ -202,8 +203,8 @@ def compress_whole_video(name, raw_clip, Q, width=256,height=256, GOP=16, frame_
                 bpp_act_list += torch.FloatTensor([bpp])
         elif frame_comb == 3:
             for g in range(raw_clip.size(0)):
-                Y1_raw = raw_clip[g][0].unsqueeze(0)
-                Y1_com = clip[g].unsqueeze(0)
+                Y1_raw = torch.cat([raw_clip[g][v].unsqueeze(0) for v in range(raw_clip.size(1))])
+                Y1_com = torch.cat([clip[g + v * raw_clip.size(0)].unsqueeze(0) for v in range(raw_clip.size(1))])
                 psnr_list += [PSNR(Y1_raw, Y1_com)]
                 msssim_list += [MSSSIM(Y1_raw, Y1_com)]
                 bpp_act_list += torch.FloatTensor([bpp])
