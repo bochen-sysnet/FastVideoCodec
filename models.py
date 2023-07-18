625fd1873895249ed1c1ca4e993e935d0c1dca43
Bo Chen <bochen1993cs@gmai.com>
2023-07-17 19:00:08 -0500

    test online finetune with 0.05 sparse tensor

--- models.py
+++ models.py
@@ -2213,7 +2213,8 @@ def replace_elements(image1, image2, r=0.01):
     # print(max_indices,compressed_size,sparse_tensor);exit(0)
 
     # Convert the difference to bytes + number of locations
-    diff_bytes = diff_elements[max_indices].cpu().detach().numpy().astype(np.float16).tobytes()
+    diff_bytes = diff_elements.cpu().detach().numpy().astype(np.float16).tobytes()
+    # diff_bytes = diff_elements[max_indices].cpu().detach().numpy().astype(np.float16).tobytes()
     # diff_bytes += np.diff(max_indices.cpu().detach().numpy()).astype(np.uint16).tobytes()
     
     # Compress the difference using zlib compression
